4245602451f33f7d8eb5d394d556665671bc98b5
Fred Pook <fred@driebit.nl>
2014-08-19 17:50:34 +0200

    [site] Added module erl

---
+++ mod_ginger_site.erl
@@ -0,0 +1,9 @@
+%% @author Driebit <info@driebit.nl>
+%% @copyright 2014
+
+-module(mod_ginger_site).
+-author("Driebit <info@driebit.nl>").
+
+-mod_title("Ginger base site").
+-mod_description("Ginger Base Site").
+-mod_prio(100).
