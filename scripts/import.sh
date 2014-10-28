621af2b50e2f1590fbbaebb4e261337ff9f648c7
Dorien <dorien@driebit.nl>
2014-10-28 14:10:39 +0100

    [admin] Import database in correct database

--- scripts/import.sh
+++ scripts/import.sh
@@ -13,6 +13,6 @@ sudo -u postgres psql -c "drop database $DB"
 sudo -u postgres psql -c "create database $DB"
 sudo -u postgres psql ginger -c "CREATE SCHEMA $SCHEMA"
 sudo -u postgres psql ginger -c "GRANT ALL ON SCHEMA $SCHEMA TO $USER"
-PGPASSWORD="$PASSWORD" psql ginger -U $USER -h $HOST -f $FILE
+PGPASSWORD="$PASSWORD" psql $DB -U $USER -h $HOST -f $FILE
 
 sudo service zotonic start
