3024c85a6919078d06b02442941eaefc1c614e65
Fred Pook <fred@driebit.nl>
2014-12-11 12:41:34 +0100

    Initial module

---
+++ mod_ginger_import.erl
@@ -0,0 +1,9 @@
+%% @author Driebit <info@driebit.nl>
+%% @copyright 2014 driebit b.v.
+
+-module(mod_ginger_import).
+-author("Driebit <info@driebit.nl>").
+
+-mod_title("Ginger import").
+-mod_description("Provides import of CSV files").
+-mod_prio(200).
