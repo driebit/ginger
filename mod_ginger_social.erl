f902df273710ea6757db49ef159f08c997d0d414
Fred Pook <fred@driebit.nl>
2014-12-11 11:41:30 +0100

    [social] Initial module

---
+++ mod_ginger_social.erl
@@ -0,0 +1,9 @@
+%% @author Driebit <info@driebit.nl>
+%% @copyright 2014
+
+-module(mod_ginger_social).
+-author("Driebit <info@driebit.nl>").
+
+-mod_title("Ginger social").
+-mod_description("Provides handling of feeds from social networks").
+-mod_prio(200).
