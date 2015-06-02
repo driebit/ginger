a8704cc2aad80f761095032d0ff7cb802d2a7441
Dorien <dorien@driebit.nl>
2015-06-02 17:28:15 +0200

    [admin] import voor nieuwe ginger vagrant

--- scripts/import.sh
+++ scripts/import.sh
@@ -4,10 +4,10 @@ USER="zotonic"
 HOST="localhost"
 DB=$1
 FILE=$2
-PASSWORD="zotonic"
+PASSWORD="ginger"
 SCHEMA="public"
 
-sudo service zotonic stop
+sudo systemctl stop zotonic
 
 sudo -u postgres psql -c "drop database $DB"
 sudo -u postgres psql -c "create database $DB ENCODING 'UTF8' TEMPLATE template0"
@@ -15,4 +15,4 @@ sudo -u postgres psql ginger -c "CREATE SCHEMA $SCHEMA"
 sudo -u postgres psql ginger -c "GRANT ALL ON SCHEMA $SCHEMA TO $USER"
 PGPASSWORD="$PASSWORD" psql $DB -U $USER -h $HOST -f $FILE
 
-sudo service zotonic start
+sudo systemctl start zotonic
