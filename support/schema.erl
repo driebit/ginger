268836f845672afe1d2e1bdf77e873e8e59b69bb
David de Boer <david@driebit.nl>
2015-08-28 12:54:49 +0200

    [base] Add manage_schema convenience functions

---
+++ support/schema.erl
@@ -0,0 +1,32 @@
+%% @doc Functions that help with manage_schema data fixtures.
+-module(schema).
+
+-include_lib("zotonic.hrl").
+
+-export([
+    file/2,
+    reset/1,
+    create_identity_if_not_exists/4]
+).
+
+%% @doc Locate file in fixtures directory
+-spec file(string(), #context{}) -> string().
+file(Filename, Context) ->
+    filename:join([z_path:site_dir(Context), "files/fixtures/", Filename]).
+
+%% @doc Reset database schema
+-spec reset(#context{}) -> ok.
+reset(Context) ->
+    z:m(),
+    z_module_manager:reinstall(z_context:site(Context), Context).
+
+%% Set username and password if not set before
+-spec create_identity_if_not_exists(atom(), string(), string(), #context{}) -> ok.
+create_identity_if_not_exists(Name, Username, Password, Context) ->
+    Resource = m_rsc:rid(Name, Context),
+    case m_identity:get(Resource, Context) of
+        undefined ->
+            m_identity:set_username_pw(Resource, Username, Password, Context);
+        _ ->
+            ok
+    end.
