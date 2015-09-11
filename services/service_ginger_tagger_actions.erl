fe9bee4e4e46ea7fe527ffbfd9e77ec166307ff1
David de Boer <david@ddeboer.nl>
2015-09-11 16:38:09 +0200

    [tagger] Improve file validation

--- services/service_ginger_tagger_actions.erl
+++ services/service_ginger_tagger_actions.erl
@@ -16,34 +16,39 @@
 process_post(_ReqData, Context) ->
     %% Look up configuration (not yet used)
 
-    MediaId = process_file(Context),
-    case z_context:get_q("rfids", Context) of
-        undefined ->
-            {error, missing_arg, "rfids"};
-        Rfids ->
-            case lookup_users(Rfids, Context) of
-                [] ->
-                    {error, syntax, "None of the RFIDs are known"};
-                Users ->
-                    case get_object(Context) of
-                        undefined ->
-                            {error, missing_arg, "object_id"};
-                        ObjectId ->
-                            lager:info(
-                                "[~p] received RFID action for RFIDs ~p (users ~p) with oject ~p",
-                                [z_context:site(Context), Rfids, Users, ObjectId]
-                            ),
-                            Response = z_notifier:first(
-                                #tagger_action{
-                                    media = MediaId,
-                                    rfids = Rfids,
-                                    users = Users,
-                                    object = ObjectId
-                                },
-                                Context
-                            ),
-                            ?DEBUG(Response),
-                            z_convert:to_json(Response)
+    case process_file(Context) of
+        {error, Message} ->
+            {error, syntax, Message};
+        {ok, MediaId} ->
+
+            case z_context:get_q("rfids", Context) of
+                undefined ->
+                    {error, missing_arg, "rfids"};
+                Rfids ->
+                    case lookup_users(Rfids, Context) of
+                        [] ->
+                            {error, syntax, "None of the RFIDs are known"};
+                        Users ->
+                            case get_object(Context) of
+                                undefined ->
+                                    {error, missing_arg, "object_id"};
+                                ObjectId ->
+                                    lager:info(
+                                        "[~p] received RFID action for RFIDs ~p (users ~p) with oject ~p",
+                                        [z_context:site(Context), Rfids, Users, ObjectId]
+                                    ),
+                                    Response = z_notifier:first(
+                                        #tagger_action{
+                                            media = MediaId,
+                                            rfids = Rfids,
+                                            users = Users,
+                                            object = ObjectId
+                                        },
+                                        Context
+                                    ),
+                                    ?DEBUG(Response),
+                                    z_convert:to_json(Response)
+                            end
                     end
             end
     end.
@@ -62,16 +67,23 @@ process_file(Context) ->
             File;
         Base64 ->
             %% Handle base64-encoded file
-            FileContents = base64:decode(Base64),
-            #upload{data=FileContents, filename="media"}
+            try
+                FileContents = base64:decode(Base64),
+                #upload{data=FileContents, filename="media"}
+            catch
+                _Type:_Reason ->
+                    %% Not a valid base64-encoded string
+                    {error, "File is not a valid base64-encoded string"}
+            end
     end,
 
     case Upload of
+        {error, _} ->
+            Upload;
         undefined ->
-            undefined;
+            {ok, undefined};
         Upload1->
-            {ok, MediaId} = m_media:insert_file(Upload1, [], Context),
-            MediaId
+            m_media:insert_file(Upload1, [], Context)
     end.
 
 %% @doc Look up users based on RFID
