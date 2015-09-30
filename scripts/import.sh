13fa63bed3c20060743dd774dd172fd6670edf01
David de Boer <david@driebit.nl>
2015-09-30 15:43:50 +0200

    Fix service command syntax

--- scripts/import.sh
+++ scripts/import.sh
@@ -9,11 +9,11 @@ SCHEMA="public"
 
 # Do service instead of systemctl to be compatible with ginger-test,
 # which is still on Wheezy
-sudo service stop zotonic
+sudo service zotonic stop
 
 sudo -u postgres psql -c "drop database $DB"
 sudo -u postgres psql -c "create database $DB ENCODING 'UTF8' TEMPLATE template0"
 sudo -u postgres psql ginger -c "GRANT ALL ON SCHEMA $SCHEMA TO $USER"
 PGPASSWORD="$PASSWORD" psql $DB -U $USER -h $HOST -f $FILE
 
-sudo service start zotonic
+sudo service zotonic start
