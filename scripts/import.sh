8aa2f7370675bfd4aeba486a62af4858d7c490b4
David de Boer <david@driebit.nl>
2015-09-30 15:34:23 +0200

    Make Zotonic start/stop compatible with Wheezy
    Our live servers still run on Wheezy.

--- scripts/import.sh
+++ scripts/import.sh
@@ -7,11 +7,13 @@ FILE=$2
 PASSWORD="ginger"
 SCHEMA="public"
 
-sudo systemctl stop zotonic
+# Do service instead of systemctl to be compatible with ginger-test,
+# which is still on Wheezy
+sudo service stop zotonic
 
 sudo -u postgres psql -c "drop database $DB"
 sudo -u postgres psql -c "create database $DB ENCODING 'UTF8' TEMPLATE template0"
 sudo -u postgres psql ginger -c "GRANT ALL ON SCHEMA $SCHEMA TO $USER"
 PGPASSWORD="$PASSWORD" psql $DB -U $USER -h $HOST -f $FILE
 
-sudo systemctl start zotonic
+sudo service start zotonic
