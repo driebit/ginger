44ea1cb1b4507b316d4a93e2ebde0af4a52834b0
David de Boer <david@driebit.nl>
2015-09-30 12:39:42 +0200

    [default] Move deprecated mod_ginger_default back to separate repo

--- modules/mod_ginger_default/mod_ginger_default.erl
+++
@@ -1,6 +0,0 @@
--module(mod_ginger_default).
--author("Driebit <info@driebit.nl>").
-
--mod_title("Ginger default module").
--mod_description("Provides the default Ginger styling").
--mod_prio(100).
--- modules/mod_ginger_default/templates/blocks/_block_view_text.tpl
+++
@@ -1,3 +0,0 @@
-<div id="{% if blk.title %}{{ blk.title|slugify }}{% else %}{{ blk.id }}{% endif %}" class="block--text {% if first %}first {% endif %}">
-    {{ blk.body|show_media }}
-</div>
