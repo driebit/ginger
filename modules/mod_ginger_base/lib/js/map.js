66d5968f05e48cb6c0a281519297f2b2c41a14d1
Danny Evenwel <danny@driebit.nl>
2015-10-16 15:07:38 +0200

    [base] maps clustering image support

--- modules/mod_ginger_base/lib/js/map.js
+++ modules/mod_ginger_base/lib/js/map.js
@@ -4,19 +4,27 @@ $.widget( "ui.googlemap", {
     _create: function() {
 
         var me = this,
-             widgetElement = $(me.element),
-             id = widgetElement.attr('id'),
-             container = widgetElement,
-             locations = jQuery.parseJSON(widgetElement.data('locations')),
-             options,
-             map,
-             bounds = new google.maps.LatLngBounds(),
-             info = new google.maps.InfoWindow({maxWidth: 250}),
-             marker,
-             icon,
-             i,
-             mc,
-             markers = [];
+            widgetElement = $(me.element),
+            id = widgetElement.attr('id'),
+            container = widgetElement,
+            locations = jQuery.parseJSON(widgetElement.data('locations')),
+            options,
+            map,
+            bounds = new google.maps.LatLngBounds(),
+            info = new google.maps.InfoWindow({maxWidth: 250}),
+            marker,
+            icon,
+            i,
+            mc,
+            mcOptions = {
+                styles: [
+                {
+                    height: 53,
+                    width: 53,
+                    url: "http://google-maps-utility-library-v3.googlecode.com/svn/trunk/markerclusterer/images/m1.png"
+                }
+            ]},
+            markers = [];
 
         options = jQuery.parseJSON(widgetElement.data('mapoptions'));
 
@@ -62,7 +70,7 @@ $.widget( "ui.googlemap", {
             markers.push(marker);
          }
 
-         mc = new MarkerClusterer(map, markers);
+         mc = new MarkerClusterer(map, markers, mcOptions);
 
          if (locations.length > 1) {
              // Center map on all locations
