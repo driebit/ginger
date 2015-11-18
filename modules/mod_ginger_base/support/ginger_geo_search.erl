f7008d879fc76328c8601488f4e2839df9b37df2
Dirk Geurs <dirk@driebit.nl>
2015-11-18 13:59:14 +0100

    Added g_geo_nearby search query

--- modules/mod_ginger_base/support/ginger_geo_search.erl
+++ modules/mod_ginger_base/support/ginger_geo_search.erl
@@ -9,7 +9,8 @@
 try_search(Context) ->
     m_search:search({ginger_geo, []}, Context).
 
-%% @doc Geo-related searches
+%% @doc Supports all the usual query model arguments
+%% Selects and filters locations data.
 search_query(#search_query{search={ginger_geo, Args}}, Context) ->
         
     BaseSearch = search_query:search(Args, Context),
@@ -26,6 +27,14 @@ search_query(#search_query{search={ginger_geo, Args}}, Context) ->
         select="rsc.id, rsc.pivot_location_lat, rsc.pivot_location_lng, rsc.pivot_category_nr",
         limit="Limit ALL",
         where=Where
+    };
+
+%% @doc Similar to z_geo_search:search_query/2 but result set includes locations and category
+search_query(#search_query{search={g_geo_nearby, Args}}, Context) ->
+    BaseSearch = z_geo_search:search_query(#search_query{search={geo_nearby, Args}}, Context),
+    BaseSearch#search_sql{
+        select="r.id, r.pivot_location_lat, r.pivot_location_lng, r.pivot_category_nr",
+        limit="Limit ALL"
     };
 
 search_query(#search_query{}, _Context) ->
