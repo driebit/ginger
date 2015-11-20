1764dc396e1ee7d569b591eb02afbf378b82f797
Dirk Geurs <dirk@driebit.nl>
2015-11-20 12:55:49 +0100

    [base] geo_search accepts ginger_search arguments

--- modules/mod_ginger_base/support/ginger_search.erl
+++ modules/mod_ginger_base/support/ginger_search.erl
@@ -1,6 +1,6 @@
 -module(ginger_search).
 
--export([search_query/2, withdefault/2]).
+-export([search_query/2, merge_ginger_args/1]).
 
 -include_lib("zotonic.hrl").
 
@@ -30,21 +30,23 @@ parse_argument({keyword, Keywords}) when is_list(Keywords) ->
 parse_argument(Arg) ->
     [Arg].
     
+%% @doc Process custom arguments and add defaults
+merge_ginger_args(Args) ->
+     DefaultArgs = [
+         % TODO: Fetch default cat_excludes from database
+         {cat_exclude, [meta, menu, admin_content_query]},
+         % TODO: Filter all resources within category if it is unfindable
+         {custompivot,"ginger_search"},
+         {filter,["is_unfindable",'=',"f"]}
+     ],
+
+     MergedArgs = lists:merge(DefaultArgs, Args),
+     MergedArgs1 = withdefault({is_published, true}, MergedArgs),
+    
+     % Parse custom ginger_search arguments
+     lists:flatmap(fun parse_argument/1, MergedArgs1).
+    
 %% @doc Supports all the usual query model arguments, adds default excludes.
 search_query(#search_query{search={ginger_search, Args}}, Context) ->
-    
-    DefaultArgs = [
-        % TODO: Fetch default cat_excludes from database
-        {cat_exclude, [meta, menu, admin_content_query]},
-        % TODO: Filter all resources within category if it is unfindable
-        {custompivot,"ginger_search"},
-        {filter,["is_unfindable",'=',"f"]}
-    ],
-
-    MergedArgs = lists:merge(DefaultArgs, Args),
-    MergedArgs1 = withdefault({is_published, true}, MergedArgs),
-    
-    % Parse custom ginger_search arguments
-    MergedArgs2 = lists:flatmap(fun parse_argument/1, MergedArgs1),
-    
-    search_query:search(MergedArgs2, Context).
+    QueryArgs = merge_ginger_args(Args),
+    search_query:search(QueryArgs, Context).
