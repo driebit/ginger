2d1333ceb83984619a06133c49bddbe1c875a4af
Dirk Geurs <dirk@driebit.nl>
2015-11-20 16:09:10 +0100

    [base] Make default search options configurable

--- modules/mod_ginger_base/support/ginger_search.erl
+++ modules/mod_ginger_base/support/ginger_search.erl
@@ -11,7 +11,13 @@ withdefault({Key, _} = Prop, Proplist) ->
             Proplist;
         false ->
             lists:append(Proplist, [Prop])
-    end.
+    end;
+    
+withdefault(Defaults, Proplist) when is_list(Defaults) ->
+    lists:foldl(
+        fun withdefault/2,
+        Proplist,
+        Defaults).
     
 %% @doc Parse custom argument into Zotonic argument
 parse_argument({keyword, Keyword}) 
@@ -27,24 +33,36 @@ parse_argument({keyword, Keywords}) when is_list(Keywords) ->
         Keywords
     );
 
+parse_argument({is_findable, Bool}) ->
+    % TODO: Filter all resources within category if it is unfindable
+    Is_unfindable = not Bool,
+    [{custompivot, "ginger_search"},
+     {filter, ["is_unfindable", Is_unfindable]}];
+     
+parse_argument({cat_exclude_defaults, Bool}) ->
+    case Bool of
+        true ->
+            [{cat_exclude, [meta, menu, admin_content_query]}];
+        false ->
+            []
+    end;            
+
 parse_argument(Arg) ->
     [Arg].
     
 %% @doc Process custom arguments and add defaults
 merge_ginger_args(Args) ->
-     DefaultArgs = [
-         % TODO: Fetch default cat_excludes from database
-         {cat_exclude, [meta, menu, admin_content_query]},
-         % TODO: Filter all resources within category if it is unfindable
-         {custompivot,"ginger_search"},
-         {filter,["is_unfindable",'=',"f"]}
-     ],
 
-     MergedArgs = lists:merge(DefaultArgs, Args),
-     MergedArgs1 = withdefault({is_published, true}, MergedArgs),
-    
-     % Parse custom ginger_search arguments
-     lists:flatmap(fun parse_argument/1, MergedArgs1).
+    % Associate default arguments
+    DefaultArgs = [
+        {is_published, true},
+        {is_findable, true},
+        {cat_exclude_defaults, true}
+    ],
+    MergedArgs = withdefault(DefaultArgs, Args),
+
+    % Parse custom ginger_search arguments
+    lists:flatmap(fun parse_argument/1, MergedArgs).
     
 %% @doc Supports all the usual query model arguments, adds default excludes.
 search_query(#search_query{search={ginger_search, Args}}, Context) ->
