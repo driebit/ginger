e956ce19171c42d9a35710d03cfd83e7317924cb
David de Boer <david@driebit.nl>
2015-11-25 22:33:26 +0100

    Add image width/height Open Graph tags
    When sharing a page for the first time, its og:image image was not shown.
    According to https://developers.facebook.com/docs/sharing/best-practices#precaching
    adding these tags will cause the image to be shown on the first share.

--- modules/mod_ginger_base/support/ginger_search.erl
+++ modules/mod_ginger_base/support/ginger_search.erl
@@ -1,8 +1,37 @@
 -module(ginger_search).
 
--export([search_query/2, merge_ginger_args/1]).
+-export([
+    search_query/2,
+    get_unfindable_categories/1,
+    merge_ginger_args/2
+]).
 
 -include_lib("zotonic.hrl").
+
+-define(GINGER_SEARCH_PIVOT, ginger_search).
+
+%% @doc Supports all the usual query model arguments, adds default excludes.
+search_query(#search_query{search={ginger_search, Args}}, Context) ->
+    QueryArgs = merge_ginger_args(Args, Context),
+    search_query:search(QueryArgs, Context).
+
+%% @doc Get categories marked unfindable that must be excluded from search results
+-spec get_unfindable_categories(#context{}) -> list().
+get_unfindable_categories(Context) ->
+    Categories = z_search:query_(
+        [
+            {cat, category},
+            {custompivot, ?GINGER_SEARCH_PIVOT},
+            {filter, ["is_unfindable", true]}
+        ],
+        Context
+    ),
+    lists:map(
+        fun(Category) ->
+            m_rsc:p(Category, name, Context)
+        end,
+        Categories
+    ).
 
 %% @doc Add property to proplist if not defined
 withdefault({Key, _} = Prop, Proplist) ->
@@ -12,36 +41,36 @@ withdefault({Key, _} = Prop, Proplist) ->
         false ->
             lists:append(Proplist, [Prop])
     end;
-    
+
 withdefault(Defaults, Proplist) when is_list(Defaults) ->
     lists:foldl(
         fun withdefault/2,
         Proplist,
         Defaults).
-    
+
 %% @doc Parse custom argument into Zotonic argument
-parse_argument({keyword, Keyword}) 
+parse_argument({keyword, Keyword}, _Context)
     when is_integer(Keyword); is_atom(Keyword) ->
         [{hasobject, [Keyword, subject]}];
 
-parse_argument({keyword, Keywords}) when is_list(Keywords) ->
+parse_argument({keyword, Keywords}, Context) when is_list(Keywords) ->
     lists:map(
-        fun(Keyword) -> 
-            [Prop|_] = parse_argument({keyword, Keyword}),
+        fun(Keyword) ->
+            [Prop|_] = parse_argument({keyword, Keyword}, Context),
             Prop
         end,
         Keywords
     );
 
-parse_argument({is_findable, Bool}) when is_boolean(Bool) ->
+parse_argument({is_findable, Bool}, _Context) when is_boolean(Bool) ->
     % TODO: Filter all resources within category if it is unfindable
     Is_unfindable = not Bool,
     [{filter, ["is_unfindable", Is_unfindable]}];
-     
-parse_argument({is_findable, Val}) ->
-    parse_argument({is_findable, z_convert:to_bool(Val)});
-    
-parse_argument({cat_exclude_defaults, Bool}) when is_boolean(Bool) ->
+
+parse_argument({is_findable, Val}, Context) ->
+    parse_argument({is_findable, z_convert:to_bool(Val)}, Context);
+
+parse_argument({cat_exclude_defaults, Bool}, _Context) when is_boolean(Bool) ->
     case Bool of
         true ->
             [{cat_exclude, [meta, menu, admin_content_query]}];
@@ -49,36 +78,44 @@ parse_argument({cat_exclude_defaults, Bool}) when is_boolean(Bool) ->
             []
     end;
 
-parse_argument({cat_exclude_defaults, Val}) ->
-    parse_argument({cat_exclude_defaults, z_convert:to_bool(Val)});
+parse_argument({cat_exclude_defaults, Val}, Context) ->
+    parse_argument({cat_exclude_defaults, z_convert:to_bool(Val)}, Context);
+
+parse_argument({cat_exclude_unfindable, Val}, Context) ->
+    case z_convert:to_bool(Val) of
+        true ->
+            [{cat_exclude, get_unfindable_categories(Context)}];
+        false ->
+            []
+    end;
 
 % Filtering on undefined is supported from Zotonic 0.13.16
-parse_argument({has_geo, true}) ->
+parse_argument({has_geo, true}, _Context) ->
     [{filter, ["pivot_location_lat", ne, undefined]},
      {filter, ["pivot_location_lng", ne, undefined]}];
-    
-parse_argument({has_geo, false}) ->
+
+parse_argument({has_geo, false}, _Context) ->
     [{filter, ["pivot_location_lat", eq, undefined]},
      {filter, ["pivot_location_lng", eq, undefined]}];
-    
-parse_argument({has_geo, Val}) ->
-    parse_argument({has_geo, z_convert:to_bool(Val)});    
 
-parse_argument({_, undefined}) ->
+parse_argument({has_geo, Val}, Context) ->
+    parse_argument({has_geo, z_convert:to_bool(Val)}, Context);
+
+parse_argument({_, undefined}, _Context) ->
     [];
-    
-parse_argument({Key, <<"undefined">>}) ->
-    parse_argument({Key, undefined});
 
-parse_argument(Arg) ->
+parse_argument({Key, <<"undefined">>}, Context) ->
+    parse_argument({Key, undefined}, Context);
+
+parse_argument(Arg, _Context) ->
     [Arg].
-    
+
 %% @doc Process custom arguments and add defaults
-merge_ginger_args(Args) ->
-    
+merge_ginger_args(Args, Context) ->
+
     % Always set these extra query arguments
     ExtraArgs = [
-        {custompivot, "ginger_search"}
+        {custompivot, ?GINGER_SEARCH_PIVOT}
     ],
     MergedArgs = lists:merge(ExtraArgs, Args),
 
@@ -86,14 +123,15 @@ merge_ginger_args(Args) ->
     DefaultArgs = [
         {is_findable, true},
         {is_published, true},
-        {cat_exclude_defaults, true}
+        {cat_exclude_defaults, true},
+        {cat_exclude_unfindable, true}
     ],
     MergedArgs1 = withdefault(DefaultArgs, MergedArgs),
 
     % Parse custom ginger_search arguments
-    lists:flatmap(fun parse_argument/1, MergedArgs1).
-    
-%% @doc Supports all the usual query model arguments, adds default excludes.
-search_query(#search_query{search={ginger_search, Args}}, Context) ->
-    QueryArgs = merge_ginger_args(Args),
-    search_query:search(QueryArgs, Context).
+    lists:flatmap(
+        fun(Arg) ->
+            parse_argument(Arg, Context)
+        end,
+        MergedArgs1
+    ).
--- modules/mod_ginger_base/templates/_html_head.tpl
+++ modules/mod_ginger_base/templates/_html_head.tpl
@@ -4,3 +4,8 @@
         "css/edit-copyrights.css"
     %}
 {% endblock %}
+
+{% if id and id.depiction %}
+    <meta property="og:image:width" content="200" /> {# Set in mod_facebook/templates/mediaclass.config #}
+    <meta property="og:image:height" content="200" />
+{% endif %}
