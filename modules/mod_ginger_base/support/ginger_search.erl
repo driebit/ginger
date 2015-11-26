64851037f240dcdc853595c3511aa0bc93be0ef5
Dirk Geurs <dirk@driebit.nl>
2015-11-26 15:01:16 +0100

    [base] parse_argument can now return a function that executes on a context to generate argument list

--- modules/mod_ginger_base/support/ginger_search.erl
+++ modules/mod_ginger_base/support/ginger_search.erl
@@ -33,83 +33,6 @@ get_unfindable_categories(Context) ->
         Categories
     ).
 
-%% @doc Add property to proplist if not defined
-withdefault({Key, _} = Prop, Proplist) ->
-    case proplists:is_defined(Key, Proplist) of
-        true ->
-            Proplist;
-        false ->
-            lists:append(Proplist, [Prop])
-    end;
-
-withdefault(Defaults, Proplist) when is_list(Defaults) ->
-    lists:foldl(
-        fun withdefault/2,
-        Proplist,
-        Defaults).
-
-%% @doc Parse custom argument into Zotonic argument
-parse_argument({keyword, Keyword}, _Context)
-    when is_integer(Keyword); is_atom(Keyword) ->
-        [{hasobject, [Keyword, subject]}];
-
-parse_argument({keyword, Keywords}, Context) when is_list(Keywords) ->
-    lists:map(
-        fun(Keyword) ->
-            [Prop|_] = parse_argument({keyword, Keyword}, Context),
-            Prop
-        end,
-        Keywords
-    );
-
-parse_argument({is_findable, Bool}, _Context) when is_boolean(Bool) ->
-    % TODO: Filter all resources within category if it is unfindable
-    Is_unfindable = not Bool,
-    [{filter, ["is_unfindable", Is_unfindable]}];
-
-parse_argument({is_findable, Val}, Context) ->
-    parse_argument({is_findable, z_convert:to_bool(Val)}, Context);
-
-parse_argument({cat_exclude_defaults, Bool}, _Context) when is_boolean(Bool) ->
-    case Bool of
-        true ->
-            [{cat_exclude, [meta, menu, admin_content_query]}];
-        false ->
-            []
-    end;
-
-parse_argument({cat_exclude_defaults, Val}, Context) ->
-    parse_argument({cat_exclude_defaults, z_convert:to_bool(Val)}, Context);
-
-parse_argument({cat_exclude_unfindable, Val}, Context) ->
-    case z_convert:to_bool(Val) of
-        true ->
-            [{cat_exclude, get_unfindable_categories(Context)}];
-        false ->
-            []
-    end;
-
-% Filtering on undefined is supported from Zotonic 0.13.16
-parse_argument({has_geo, true}, _Context) ->
-    [{filter, ["pivot_location_lat", ne, undefined]},
-     {filter, ["pivot_location_lng", ne, undefined]}];
-
-parse_argument({has_geo, false}, _Context) ->
-    [{filter, ["pivot_location_lat", eq, undefined]},
-     {filter, ["pivot_location_lng", eq, undefined]}];
-
-parse_argument({has_geo, Val}, Context) ->
-    parse_argument({has_geo, z_convert:to_bool(Val)}, Context);
-
-parse_argument({_, undefined}, _Context) ->
-    [];
-
-parse_argument({Key, <<"undefined">>}, Context) ->
-    parse_argument({Key, undefined}, Context);
-
-parse_argument(Arg, _Context) ->
-    [Arg].
-
 %% @doc Process custom arguments and add defaults
 merge_ginger_args(Args, Context) ->
 
@@ -129,9 +52,92 @@ merge_ginger_args(Args, Context) ->
     MergedArgs1 = withdefault(DefaultArgs, MergedArgs),
 
     % Parse custom ginger_search arguments
-    lists:flatmap(
+    ?DEBUG(lists:flatmap(
         fun(Arg) ->
-            parse_argument(Arg, Context)
+            case parse_argument(Arg) of
+                F when is_function(F) ->
+                    F(Context);
+                L when is_list(L) ->
+                    L
+            end
         end,
         MergedArgs1
-    ).
+    )).
+
+%% @doc Add property to proplist if not defined
+withdefault({Key, _} = Prop, Proplist) ->
+    case proplists:is_defined(Key, Proplist) of
+        true ->
+            Proplist;
+        false ->
+            lists:append(Proplist, [Prop])
+    end;
+
+withdefault(Defaults, Proplist) when is_list(Defaults) ->
+    lists:foldl(
+        fun withdefault/2,
+        Proplist,
+        Defaults).
+
+%% @doc Parse custom argument into Zotonic argument
+parse_argument({keyword, Keyword})
+    when is_integer(Keyword); is_atom(Keyword) ->
+        [{hasobject, [Keyword, subject]}];
+
+parse_argument({keyword, Keywords}) when is_list(Keywords) ->
+    lists:map(
+        fun(Keyword) ->
+            [Prop|_] = parse_argument({keyword, Keyword}),
+            Prop
+        end,
+        Keywords
+    );
+
+parse_argument({is_findable, Bool}) when is_boolean(Bool) ->
+    Is_unfindable = not Bool,
+    [{filter, ["is_unfindable", Is_unfindable]}];
+
+parse_argument({is_findable, Val}) ->
+    parse_argument({is_findable, z_convert:to_bool(Val)});
+
+parse_argument({cat_exclude_defaults, Bool}) when is_boolean(Bool) ->
+    case Bool of
+        true ->
+            [{cat_exclude, [meta, menu, admin_content_query]}];
+        false ->
+            []
+    end;
+
+parse_argument({cat_exclude_defaults, Val}) ->
+    parse_argument({cat_exclude_defaults, z_convert:to_bool(Val)});
+
+parse_argument({cat_exclude_unfindable, Val}) ->
+    fun(Context) ->
+        case z_convert:to_bool(Val) of
+            true ->
+                [{cat_exclude, get_unfindable_categories(Context)}];
+            false ->
+                []
+        end
+    end;    
+
+% Filtering on undefined is supported from Zotonic 0.13.16
+parse_argument({has_geo, true}) ->
+    [{filter, ["pivot_location_lat", ne, undefined]},
+     {filter, ["pivot_location_lng", ne, undefined]}];
+
+parse_argument({has_geo, false}) ->
+    [{filter, ["pivot_location_lat", eq, undefined]},
+     {filter, ["pivot_location_lng", eq, undefined]}];
+
+parse_argument({has_geo, Val}) ->
+    parse_argument({has_geo, z_convert:to_bool(Val)});
+
+parse_argument({_, undefined}) ->
+    [];
+
+parse_argument({Key, <<"undefined">>}) ->
+    parse_argument({Key, undefined});
+
+parse_argument(Arg) ->
+    [Arg].
