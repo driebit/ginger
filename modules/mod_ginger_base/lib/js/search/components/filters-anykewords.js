e664fef9a270e65a58186deeee4d4ea6cafc820a
Dirk Geurs <dirk@driebit.nl>
2016-01-19 11:19:40 +0100

    Added anykeyword filter widget

---
+++ modules/mod_ginger_base/lib/js/search/components/filters-anykewords.js
@@ -0,0 +1,35 @@
+$.widget("ui.search_cmp_filters_keywords", {
+
+    _create: function() {
+
+        var me = this,
+            widgetElement = $(me.element),
+            inputs = widgetElement.find('input');
+
+        me.widgetElement = widgetElement;
+
+        inputs.on('change', function() {
+            $(document).trigger('search:inputChanged');
+        });
+
+    },
+
+    getValues: function() {
+
+        var me = this,
+            inputs = me.widgetElement.find('input:checked'),
+            values;
+  
+        values = $.map(inputs, function( input ) {
+            return parseInt($(input).val());
+        });
+
+        //must return an array of objects
+        return [{
+                'type': 'anykeyword',
+                'values': values
+            }
+        ]
+    }
+
+});
