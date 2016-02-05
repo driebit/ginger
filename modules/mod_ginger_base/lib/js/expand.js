cf605824c916e8e00499c8dad77d25a86f0638ec
Dorien <dorien@driebit.nl>
2016-02-05 15:35:58 +0100

    [base] added facebook widget, publication list item with expand widgets

---
+++ modules/mod_ginger_base/lib/js/expand.js
@@ -0,0 +1,49 @@
+(function($) {
+    'use strict';
+
+    $.widget('ui.expand', {
+        _create: function() {
+            var me = this,
+                element = $(me.element),
+                type = element.data('type'),
+                parentAll = '.' + element.data('parent'),
+                parent = '#' + element.data('parent'),
+                contentAll = '.' + element.data('content'),
+                contentId = '#' + element.data('content');
+
+                //alert($(window).width());
+
+            if (type == 'all') {
+                element.on('click', function (evt) {
+                    evt.preventDefault();
+
+                    console.log(parentAll);
+
+                    if (element.hasClass('is-open')) {
+                        element.removeClass('is-open');
+                        $(parentAll).removeClass('is-open');
+                        $(contentAll).slideUp();
+
+                        $(document).trigger('foldout:collapse');
+                    } else {
+                        element.addClass('is-open');
+                        $(parentAll).addClass('is-open');
+                        $(contentAll).slideDown();
+
+                        $(document).trigger('foldout:expand');
+                    }
+                });
+
+            } else {
+                element.on('click', function (evt) {
+                    evt.preventDefault();
+
+                    $(parent).toggleClass('is-open');
+                    $(element).toggleClass('is-open');
+                    $(contentId).slideToggle();
+                });
+
+            }
+        }
+    });
+})(jQuery);
