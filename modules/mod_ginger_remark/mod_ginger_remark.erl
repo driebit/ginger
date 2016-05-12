58a64325d33b48ed0ab6b40b0d0082c9deb28fb7
Dorien <dorien@driebit.nl>
2016-05-12 17:59:38 +0200

    [remark] added ginger remark

---
+++ modules/mod_ginger_remark/mod_ginger_remark.erl
@@ -0,0 +1,10 @@
+%% @author Driebit <tech@driebit.nl>
+%% @copyright 2016
+
+-module(mod_ginger_remark).
+-author("Driebit <tech@driebit.nl>").
+
+-mod_title("Ginger remark").
+-mod_description("Enables simple adding of resources.").
+
+-include_lib("zotonic.hrl").
