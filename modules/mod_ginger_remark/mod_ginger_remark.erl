f2cf186c5bea6f30b82e23f7de5784ae70642571
Dirk Geurs <dirk@driebit.nl>
2016-05-18 11:22:30 +0200

    Initial schema

--- modules/mod_ginger_remark/mod_ginger_remark.erl
+++ modules/mod_ginger_remark/mod_ginger_remark.erl
@@ -8,3 +8,46 @@
 -mod_description("Enables simple adding of resources.").
 
 -include_lib("zotonic.hrl").
+
+-mod_prio(500).
+-mod_schema(1).
+
+-export([manage_schema/2]).
+
+manage_schema(_Version, Context) ->
+  m_config:set_value(i18n, language, nl, Context),
+  #datamodel{
+    categories=[
+        {remark, text, [
+            {title, {trans, [{nl, <<"Opmerking">>},
+                             {en, <<"Remark">>}]}},
+            {language, [en,nl]}
+        ]}
+    ],
+    predicates=[
+        {has_remark,
+            [
+                {title, {trans, [
+                    {nl, <<"Heeft opmerking">>},
+                    {en, <<"Has remark">>}
+                ]}}
+            ],
+            [
+            ]
+        }
+    ],
+    resources=[
+    ],
+    media=[
+    ],
+    edges=[
+    ],
+    data = [
+        {acl_rules, [
+            {rsc, [
+                {acl_user_group_id, acl_user_group_members},
+                {actions, [insert]},
+                {category_id, remark}
+            ]}
+        ]}
+    ]}.
