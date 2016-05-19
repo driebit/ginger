d379b0c87f3c64ef814711d4b65fb9a2a26cfda4
Dirk Geurs <dirk@driebit.nl>
2016-05-19 10:59:30 +0200

    [mod_ginger_edit] Added alternate version of rscform submit event.
    The main difference with the controller_admin_edit version is the on_success
    action that replaces the view_location redirect. Very useful for front-end
    editing that happens within a page. Example of a wire:
    
    {% wire id="rscform" type="submit"
            postback={rscform on_success={alert text="word"}}
            delegate="mod_ginger_edit" %}

--- modules/mod_ginger_edit/mod_ginger_edit.erl
+++ modules/mod_ginger_edit/mod_ginger_edit.erl
@@ -36,7 +36,39 @@ event(#postback_notify{message="feedback", trigger="dialog-connect-find", target
     z_render:wire([
         {remove_class, [{target, TargetId}, {class, "loading"}]},
         {update, [{target, TargetId}, {template, "_action_dialog_connect_tab_find_results.tpl"} | Vars]}
-    ], Context).
+    ], Context);
+
+%% @doc Custom version of controller_admin_edit rscform that executes actions instead of redirecting
+event(#submit{message=rscform} = Msg, Context) ->
+    event(Msg#submit{message={rscform, []}}, Context);
+event(#submit{message={rscform, Args}}, Context) ->
+    Post = z_context:get_q_all_noz(Context),
+    Props = controller_admin_edit:filter_props(Post),
+    Id = z_convert:to_integer(proplists:get_value("id", Props)),
+    Props1 = proplists:delete("id", Props),
+    Props2 = z_notifier:foldl(#admin_rscform{id=Id, is_a=m_rsc:is_a(Id, Context)}, Props1, Context),
+    try
+        {ok, _} = m_rsc:update(Id, Props2, Context),
+        SuccessActions = proplists:get_all_values(on_success, Args),
+        z_render:wire(SuccessActions, Context)
+    catch
+        throw:{error, duplicate_uri} ->
+            z_render:growl_error("Error, duplicate uri. Please change the uri.", Context);
+        throw:{error, duplicate_page_path} ->
+            z_render:growl_error("Error, duplicate page path. Please change the uri.", Context);
+        throw:{error, duplicate_name} ->
+            z_render:growl_error("Error, duplicate name. Please change the name.", Context);
+        throw:{error, eacces} ->
+            z_render:growl_error("You don't have permission to edit this page.", Context);
+        throw:{error, invalid_query} ->
+            z_render:growl_error("Your search query is invalid. Please correct it before saving.", Context);
+        throw:{error, Message} when is_list(Message); is_binary(Message) ->
+            z_render:growl_error(Message, Context);
+        X:Y ->
+            Stacktrace = erlang:get_stacktrace(),
+            lager:error("Rsc update error: ~p:~p stacktrace: ~p", [X, Y, Stacktrace]),
+            z_render:growl_error("Something went wrong. Sorry.", Context)
+    end.
 
 %% @doc Attempt to add edges defined as form fields
 observe_admin_rscform(#admin_rscform{id=RscId}, Props, Context) ->
@@ -58,3 +90,4 @@ maybe_edge(SubjectId, PredStr, ObjectId, Context) ->
         {_, undefined} -> ok;
         {_, _} -> m_edge:insert(Subject, Pred, Object, Context)
     end.
+        
