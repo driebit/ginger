1cb0e7de13be9160ba0ea3061b0a2a0273582b21
gunnar-driebit <gunnar@driebit.nl>
2016-06-23 17:13:58 +0200

    make menu dropdowns work on tablets (#122)
    make menu dropdowns work on tablets Fix #123

--- modules/mod_ginger_base/lib/js/global-nav.js
+++ modules/mod_ginger_base/lib/js/global-nav.js
@@ -18,6 +18,33 @@
 
             $(document).on('menu:close', $.proxy(me._closeMenu, me));
 
+            if ('ontouchstart' in document.documentElement) {
+                if ($('.global-nav__menu__dropdown').length > 0) {
+                    var openLink = false;
+                    $('body').prepend('<div class="global-nav__menu__dropdown_close"> </div>');
+                    $('.global-nav__menu__dropdown_close').on('touchstart', function(e) {
+                        $('.global-nav__menu__dropdown').removeClass('touch-open');
+                        $(this).hide();
+                        openLink = false;
+                    });
+                    $('.global-nav__menu__dropdown').each(function(i) {
+                        $(this).find('a:first').on('touchstart', function(e) {
+                            if (openLink == false) {
+                                e.preventDefault();
+                                $(this).parent().addClass('touch-open');
+                                $('.global-nav__menu__dropdown_close').show();
+                                openLink = true;
+                            } else {
+                                if ($(this).parent().hasClass('touch-open') == false) {
+                                    $('.global-nav__menu__dropdown').removeClass('touch-open');
+                                    e.preventDefault();
+                                    $(this).parent().addClass('touch-open');
+                                }
+                            }
+                        });
+                    });
+                }
+            }
         },
 
         _closeMenu: function(event) {
