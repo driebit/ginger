0a679844aad823ae39d0be1da2c82bb02c7af80a
Ronald Benschop <ronald@driebit.nl>
2016-08-04 15:50:14 +0200

    Added dynamic decode client

--- modules/mod_ginger_github/support/ginger_http_client.erl
+++ modules/mod_ginger_github/support/ginger_http_client.erl
@@ -5,17 +5,32 @@
 
 -export([
     request/2,
-    request/4
+    request/4,
+    request/5
 ]).
 
 -spec request(string(), list()) -> list().
 request(Url, Headers) ->
-    handle_response(httpc:request(get, {Url, Headers}, [], []), Url).
+    case handle_response(httpc:request(get, {Url, Headers}, [], []), Url) of
+        {error, _} ->
+            undefined;
+        ResponseBody ->
+            mochijson2:decode(ResponseBody)
+    end.
 
 -spec request(string(), string(), list(), list()) -> list().
 request(RequestMethod, Url, Headers, Data) ->
+    request(RequestMethod, Url, Headers, Data, fun mochijson2:decode/1).
+
+-spec request(string(), string(), list(), list(), fun()) -> list().
+request(RequestMethod, Url, Headers, Data, DecodeFun) ->
     JsonData = encode_data(Data),
-    handle_response(httpc:request(RequestMethod, {Url, Headers, "application/json", JsonData}, [], []), Url).
+    case handle_response(httpc:request(RequestMethod, {Url, Headers, "application/json", JsonData}, [], []), Url) of
+        {error, _} ->
+            undefined;
+        ResponseBody ->
+            DecodeFun(ResponseBody)
+    end.
 
 handle_response(Response, Url) ->
     case Response of
@@ -25,16 +40,16 @@ handle_response(Response, Url) ->
             Body
         }} when StatusCode >= 400 ->
             lager:error("~p error ~p for URL ~p: ~p", [?MODULE, StatusCode, Url, Body]),
-            undefined;
+            {error, {StatusCode, Body}};
         {ok, {
             {_HTTP, _StatusCode, _OK},
             _Headers,
             Body
         }} ->
-            mochijson2:decode(Body);
+            Body;
         JsonResponse ->
             lager:error("~p unknown error for URL ~p: ~p", [?MODULE, Url, JsonResponse]),
-            undefined
+            {error, JsonResponse}
     end.
 
 encode_data(Data) ->
