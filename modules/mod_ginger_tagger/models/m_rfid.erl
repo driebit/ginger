544ed03c420f3e146d1fa455d707a610574d8282
David de Boer <david@ddeboer.nl>
2016-08-23 12:32:47 +0200

    [tagger] Convert between endianness

--- modules/mod_ginger_tagger/models/m_rfid.erl
+++ modules/mod_ginger_tagger/models/m_rfid.erl
@@ -5,15 +5,22 @@
 -export([
     get/2,
     add/3,
-    delete/3
+    delete/3,
+    convert_endian/1
 ]).
 
 -include_lib("zotonic.hrl").
 
 %% @doc Get RFID identity by number
--spec get(string(), #context{}) -> list() | undefined.
+-spec get(binary(), #context{}) -> list(tuple()) | undefined.
 get(Rfid, Context) ->
-    m_identity:lookup_by_type_and_key(rfid, normalise(Rfid), Context).
+    case m_identity:lookup_by_type_and_key(rfid, normalise(Rfid), Context) of
+        undefined ->
+            %% Fall back to other endianness
+            m_identity:lookup_by_type_and_key(rfid, convert_endian(normalise(Rfid)), Context);
+        Identity ->
+            Identity
+    end.
 
 %% @doc Add RFID identity to a resource
 -spec add(m_rsc:resource(), binary(), #context{}) -> {error, string()} | {ok, pos_integer()}.
@@ -37,3 +44,17 @@ delete(Resource, Rfid, Context) ->
 -spec normalise(binary()) -> binary().
 normalise(Rfid) ->
     z_string:to_lower(Rfid).
+
+%% @doc Convert between little-endian and big-endian (9C9C25C5 <-> C5259C9C).
+%%      This is a fallback for RFID chips that were encoded in little instead of
+%%      big endian.
+-spec convert_endian(binary()) -> binary().
+convert_endian(HexadecimalRfid) when size(HexadecimalRfid) rem 2 =:= 0 ->
+    convert_endian(HexadecimalRfid, <<>>);
+convert_endian(HexadecimalRfid) ->
+    HexadecimalRfid.
+
+convert_endian(<<>>, Acc) ->
+    Acc;
+convert_endian(<<Head:2/binary, Tail/binary>>, Acc) ->
+    convert_endian(Tail, <<Head/binary, Acc/binary>>).
