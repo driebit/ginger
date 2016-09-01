12e0720eb61a332a2739bd574f50986e85940809
David de Boer <david@ddeboer.nl>
2016-09-01 10:17:44 +0200

    [rdf] Add CORS support (fix #166)

--- modules/mod_ginger_rdf/controllers/controller_rdf.erl
+++ modules/mod_ginger_rdf/controllers/controller_rdf.erl
@@ -20,7 +20,13 @@ service_available(ReqData, DispatchArgs) when is_list(DispatchArgs) ->
     Context  = z_context:new(ReqData, ?MODULE),
     Context1 = z_context:set(DispatchArgs, Context),
     z_context:lager_md(Context1),
-    ?WM_REPLY(true, Context1).
+    ReqData1 = set_cors_headers(ReqData, Context1),
+    case wrq:method(ReqData1) of
+        'OPTIONS' ->
+            {{halt, 204}, ReqData1, Context1};
+        _ ->
+            {true, ReqData1, Context1}
+    end.
 
 is_authorized(ReqData, Context) ->
     Context1 = ?WM_REQ(ReqData, Context),
@@ -41,3 +47,24 @@ json_ld(ReqData, Context) ->
             mochijson2:encode(JsonLd)
     end,
     ?WM_REPLY(Result, Context1).
+
+%% @doc Always enable CORS for this controller.
+%%      Unlike controller_api:set_cors_header, this function doesn't look at the
+%%      configuration but unconditionally exposes this controller between
+%%      domains. This makes sharing content much easier.
+set_cors_headers(ReqData, Context) ->
+    lists:foldl(
+        fun ({K, Def}, Acc) ->
+            case m_config:get_value(site, K, Def, Context) of
+                undefined ->
+                    Acc;
+                V ->
+                    wrq:set_resp_header(z_convert:to_list(K), z_convert:to_list(V), Acc)
+            end
+        end,
+        ReqData,
+        [
+            {'Access-Control-Allow-Origin', "*"},
+            {'Access-Control-Max-Age', undefined}
+        ]
+    ).
