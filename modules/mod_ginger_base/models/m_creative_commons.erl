def7ba1b7f24b9e8c83272b7a2bc2b291486f1f9
David de Boer <david@ddeboer.nl>
2016-09-09 10:22:33 +0200

    [base] Fix Creative Commons link for undefined license

--- modules/mod_ginger_base/models/m_creative_commons.erl
+++ modules/mod_ginger_base/models/m_creative_commons.erl
@@ -5,7 +5,8 @@
     m_find_value/3,
     m_to_list/2,
     m_value/2,
-    url_for/1
+    url_for/1,
+    language_url_for/2
 ]).
 
 -include_lib("zotonic.hrl").
@@ -17,11 +18,7 @@ m_find_value(License, #m{value = undefined} = M, _Context) ->
 m_find_value(url, #m{value = License}, _Context) ->
     url_for(License);
 m_find_value(language_url, #m{value = License}, Context) ->
-    iolist_to_binary([
-        url_for(License),
-        <<"/deed.">>,
-        z_convert:to_binary(z_context:language(Context))
-    ]).
+    language_url_for(License, Context).
 
 m_to_list(_, _Context) ->
     [].
@@ -29,6 +26,8 @@ m_to_list(_, _Context) ->
 m_value(_Source, _Context) ->
     undefined.
 
+%% @doc Get URL to license at the Creative Commons website
+-spec url_for(binary() | string()) -> binary() | undefined.
 url_for(License) when is_list(License) ->
     url_for(z_convert:to_binary(License));
 url_for(<<"BY">>) ->
@@ -49,3 +48,18 @@ url_for(<<"PD">>) ->
     <<"http://creativecommons.org/publicdomain/mark/1.0">>;
 url_for(_) ->
     undefined.
+
+%% @doc Get URL to translated license at the Creative Commons website
+-spec language_url_for(binary(), #context{}) -> binary() | undefined.
+language_url_for(License, Context) ->
+    with_language(url_for(License), Context).
+
+-spec with_language(binary() | undefined, #context{}) -> binary() | undefined.
+with_language(undefined, _Context) ->
+    undefined;
+with_language(Url, Context) ->
+    iolist_to_binary([
+        Url,
+        <<"/deed.">>,
+        z_convert:to_binary(z_context:language(Context))
+    ]).
