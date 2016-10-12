0644a45e1213a249540418352c04f028cde53349
Finn Kruyning <finn@driebit.nl>
2016-10-12 17:38:18 +0200

    More Carousel Block in qonqer / list-item

--- modules/mod_ginger_base/lib/js/carousel.js
+++ modules/mod_ginger_base/lib/js/carousel.js
@@ -1,12 +1,14 @@
 $.widget("ui.carousel", {
 
     _create: function () {
-
         var me = this,
             widgetElement = $(me.element),
             id = widgetElement.attr('id'),
-            options = this.options;
+            options = {dots: true, autoplay: true}
+            if (widgetElement.data('carousel-options')) {
+                options =  widgetElement.data('carousel-options');
 
+            }
         if (options) {
             $(widgetElement).slick(options);
         } else {
