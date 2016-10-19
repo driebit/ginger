4d8cc2a1adad4c32597fab89c97eef66918df64a
Finn Kruyning <finn@driebit.nl>
2016-10-19 18:17:09 +0200

    WIP Carousel slick options

--- modules/mod_ginger_base/lib/js/carousel.js
+++ modules/mod_ginger_base/lib/js/carousel.js
@@ -4,10 +4,12 @@ $.widget("ui.carousel", {
         var me = this,
             widgetElement = $(me.element),
             id = widgetElement.attr('id'),
-            options = {dots: true, autoplay: true}
+            options = {dots: true, autoplay: true};
+
+            // options = this.options; // old code
+
             if (widgetElement.data('carousel-options')) {
                 options =  widgetElement.data('carousel-options');
-
             }
         if (options) {
             $(widgetElement).slick(options);
