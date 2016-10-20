97a0dd760e4c33d09b820fd681dd24c38ad31b32
Loetie Kruger <loetie@driebit.nl>
2016-10-20 10:22:01 +0200

    [base] Added extra options to carousel js. (Qonqer/Finn)

--- modules/mod_ginger_base/lib/js/carousel.js
+++ modules/mod_ginger_base/lib/js/carousel.js
@@ -1,21 +1,41 @@
 $.widget("ui.carousel", {
 
     _create: function () {
+        function MergeRecursive(obj1, obj2) {
+            for (var p in obj2) {
+                try {
+                    // Property in destination object set; update its value.
+                    if ( obj2[p].constructor==Object ) {
+                        obj1[p] = MergeRecursive(obj1[p], obj2[p]);
+                    } else {
+                        obj1[p] = obj2[p];
+                    }
+                } catch(e) {
+                    // Property in destination object not set; create it and set its value.
+                    obj1[p] = obj2[p];
+                }
+            }
+            return obj1;
+        }
+
         var me = this,
             widgetElement = $(me.element),
             id = widgetElement.attr('id'),
-            options = {dots: true, autoplay: true};
+            settings = this.options;  // Is this used?
 
-            // options = this.options; // old code
+        settings = MergeRecursive(settings, {dots: true, autoplay: true}); // this would be default behavior
 
-            if (widgetElement.data('carousel-options')) {
-                options =  widgetElement.data('carousel-options');
-            }
-        if (options) {
-            $(widgetElement).slick(options);
+        if (widgetElement.data('carousel-options')) {
+            var options =  widgetElement.data('carousel-options');
+            settings = MergeRecursive(settings, options);
+        }
+
+        if (settings) {
+            $(widgetElement).slick(settings);
         } else {
             $(widgetElement).slick();
         }
+
     }
 
 });
