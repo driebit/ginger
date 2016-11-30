driebit/ginger

commit 2a53576d35ccf83bd1e739eb68120fad31a170cd

[remark] Don't link to the anonymous user (#195)

modules/mod_ginger_remark/templates/remark/remark-view.tpl
@@ -8,24 +8,19 @@
     <div class="remark-item {{ extraClasses }}" id="remark-{{ remark_id }}">
         <article>
             <div class="remark-item__author">
-                    {% if remark_id.o.author as author %}
-                        {% include "avatar/avatar.tpl" id=author %}
-                    {% elseif remark_id.anonymous_name %}
-                        {% image m.rsc.fallback.id mediaclass="avatar" class="avatar__image" %}
-                    {% else %}
-                        {% include "avatar/avatar.tpl" id=m.rsc[remark_id.creator_id] %}
-                    {% endif %}
-                <div class="remark-item__author__text">
-
+               <div class="remark-item__author__text">
                     {% if remark_id.o.author|default:remark_id.creator_id as author %}
-                        <b><a href="{{ m.rsc[author].page_url }}">{{ m.rsc[author].title }}</a></b>
-                    {% else %}
-                        {% if remark_id.anonymous_email_visible %}
-                             <a href="click.to.mail" address="{{ remark_id.anonymous_email|mailencode }}" class="do_mail_decode">{{ remark_id.anonymous_name }}</a>
+                        {% if author.name == "human_user" %}
+                            {% if remark_id.anonymous_email_visible %}
+                                 <a href="click.to.mail" address="{{ remark_id.anonymous_email|mailencode }}" class="do_mail_decode">{{ remark_id.anonymous_name }}</a>
+                            {% else %}
+                                {{ remark_id.anonymous_name }}
+                            {% endif %}
                         {% else %}
-                            {{ remark_id.anonymous_name }}
+                            <b><a href="{{ m.rsc[author].page_url }}">{{ m.rsc[author].title }}</a></b>
                         {% endif %}
                     {% endif %}
+
                     <time datetime="{{ remark_id.created|date:"Y-F-jTH:i" }}">
                         {% block datetime %}{{ remark_id.created|date:"d F Y" }}{% endblock %}
                     </time>

commit bc49c45378f87bfb67bceb641faad3fee1eada6a

[remark] Fix e-mail link missing for anonymous user
* This was broken in #195.
* This PR drops the fallback to creator.
* Remain compatible with anonymous remarks imported from Anymeta.

modules/mod_ginger_remark/templates/remark/remark-view.tpl
@@ -9,18 +9,22 @@
         <article>
             <div class="remark-item__author">
                <div class="remark-item__author__text">
-                    {% if remark_id.o.author|default:remark_id.creator_id as author %}
+                    {% if remark_id.o.author as author %}
+                        {# Do not show link for anonymous remarks imported from
+                           Anymeta, which have an author named human_user #}
                         {% if author.name == "human_user" %}
-                            {% if remark_id.anonymous_email_visible %}
-                                 <a href="click.to.mail" address="{{ remark_id.anonymous_email|mailencode }}" class="do_mail_decode">{{ remark_id.anonymous_name }}</a>
-                            {% else %}
-                                {{ remark_id.anonymous_name }}
-                            {% endif %}
+                            {{ author.title }}
                         {% else %}
-                            <b><a href="{{ m.rsc[author].page_url }}">{{ m.rsc[author].title }}</a></b>
+                            <a href="{{ author.page_url }}">{{ author.title }}</a>
+                        {% endif %}
+                    {% else %}
+                        {% if remark_id.anonymous_email_visible %}
+                           <a href="click.to.mail" address="{{ remark_id.anonymous_email|mailencode }}"
+                              class="do_mail_decode">{{ remark_id.anonymous_name }}</a>
+                        {% else %}
+                            {{ remark_id.anonymous_name }}
                         {% endif %}
                     {% endif %}
-
                     <time datetime="{{ remark_id.created|date:"Y-F-jTH:i" }}">
                         {% block datetime %}{{ remark_id.created|date:"d F Y" }}{% endblock %}
                     </time>