7cc269a19dfb19ae634be8442984bd89cef24f2c
Dorien Drees <dorien@driebit.nl>
2017-02-09 14:48:58 +0100

    [collection] conflict

--- mod_beeldenzoeker.erl
+++ mod_beeldenzoeker.erl
@@ -5,7 +5,7 @@
 -mod_description("Beeldenzoeker module om digitale collecties in te zien").
 -mod_prio(200).
 -mod_depends([mod_ginger_base]).
--mod_schema(2).
+-mod_schema(3).
 
 -include_lib("zotonic.hrl").
 
@@ -14,29 +14,32 @@
 ]).
 
 manage_schema(_Version, _Context) ->
-    #datamodel{
-        categories = [
-        ],
-        resources = [
-            {beeldenzoeker, text, [
-                {title, {trans, [{nl, <<"Beeldenzoeker">>},
-                    {en, <<"Media discovery">>}]}},
-                {language, [en, nl]},
-                {is_unfindable, true}
-            ]}
-        ],
-        edges = [
-        ],
-        predicates = [
-        ],
-        data = [
-            % {acl_rules, [
-            %     % {rsc, [
-            %     %     {acl_user_group_id, acl_user_group_members},
-            %     %     {actions, [insert, update, link, delete]},
-            %     %     {category_id, project},
-            %     %     {is_owner, true}
-            %     % ]}
-            % ]}
-        ]
-    }.
+  #datamodel{
+    categories=[
+    ],
+    resources=[
+    	{beeldenzoeker, text, [
+          {title, {trans, [{nl, <<"Beeldenzoeker">>},
+                           {en, <<"Media discovery">>}]}},
+          {language, [en,nl]},
+          {is_unfindable, true}
+      ]},
+      {beeldenzoeker, menu, [
+          {title, "Beeldenzoeker menu"}
+      ]}
+    ],
+    edges=[
+    ],
+    predicates=[
+    ],
+    data = [
+        % {acl_rules, [
+        %     % {rsc, [
+        %     %     {acl_user_group_id, acl_user_group_members},
+        %     %     {actions, [insert, update, link, delete]},
+        %     %     {category_id, project},
+        %     %     {is_owner, true}
+        %     % ]}
+        % ]}
+    ]
+  }.
