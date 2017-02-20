6a3b447fbefabfacc8f30f4f8af9fe7c5c29e2ff
David de Boer <david@ddeboer.nl>
2017-02-20 18:02:09 +0100

    [adlib_elasticsearch] Configure index config param on module init
    So we can rely on the value being available in all templates.

--- modules/mod_ginger_adlib_elasticsearch/mod_ginger_adlib_elasticsearch.erl
+++ modules/mod_ginger_adlib_elasticsearch/mod_ginger_adlib_elasticsearch.erl
@@ -8,11 +8,15 @@
 -mod_depends([mod_elasticsearch, mod_ginger_adlib]).
 
 -export([
+    init/1,
     observe_adlib_update/2
 ]).
 
 -include_lib("mod_ginger_adlib/include/ginger_adlib.hrl").
 -include_lib("zotonic.hrl").
+
+init(Context) ->
+    default_config(index, index(Context), Context).
 
 observe_adlib_update(#adlib_update{date = _Date, database = Database, record = #{<<"priref">> := Priref} = Record}, Context) ->
     lager:info("Indexing Adlib record ~s", [Priref]),
@@ -33,4 +37,12 @@ index(Context) ->
         undefined -> Default;
         <<>> -> Default;
         Value -> z_convert:to_binary(Value)
+    end.
+
+default_config(Key, Value, Context) ->
+    case m_config:get_value(?MODULE, Key, Context) of
+        undefined ->
+            m_config:set_value(?MODULE, Key, Value, Context);
+        _ ->
+            ok
     end.
--- modules/mod_ginger_adlib_elasticsearch/templates/_admin_configure_module.tpl
+++ modules/mod_ginger_adlib_elasticsearch/templates/_admin_configure_module.tpl
@@ -1,7 +1,7 @@
 <div class="modal-body">
     <div class="form-group">
         <label for="index">{_ Elasticsearch index _}</label>
-        <input type="text" class="form-control do_autofocus" id="index" placeholder="{{ m.config.mod_ginger_adlib_elasticsearch.index.value|default:m.site.host }}_adlib" value="{{ m.config.mod_ginger_adlib_elasticsearch.index.value|escape }}">
+        <input type="text" class="form-control do_autofocus" id="index" value="{{ m.config.mod_ginger_adlib_elasticsearch.index.value|escape }}">
         {% wire id="index" type="blur" action={config_toggle module="mod_ginger_adlib_elasticsearch" key="index" on="keyup"} %}
     </div>
 </div>
