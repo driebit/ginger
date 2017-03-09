76deda74ae6a9c8e0a2661d598cc1d00242701a9
David de Boer <david@ddeboer.nl>
2017-03-09 09:36:48 +0100

    [ginger_adlib_elasticsearch] Allow full entries to be matched in mappers

--- modules/mod_ginger_adlib_elasticsearch/support/ginger_adlib_elasticsearch_mapper.erl
+++ modules/mod_ginger_adlib_elasticsearch/support/ginger_adlib_elasticsearch_mapper.erl
@@ -10,45 +10,47 @@
 %% @doc Maps an Adlib record to an Elasticsearch document, taking care of nested
 %%      fields. As Mapping, pass a module that implements behaviour
 %%      ginger_adlib_elasticsearch_mapping.
--spec map(map(), module()) -> term() | map().
+-spec map(map(), module()) -> map().
 map(Map, Mapping) ->
     maps:fold(
-        fun(Key, Value, Acc) when is_map(Value) ->
-            Mapped = map(Value, Mapping),
-            map_property(Key, Mapped, Acc, Mapping);
-        (Key, Value, Acc) ->
-            %% when is_map(Value)
-            map_property(Key, Value, Acc, Mapping)
+        fun(Key, Value, Acc) ->
+            map_value(Key, Value, Acc, Mapping)
         end,
         #{},
         Map
     ).
 
+map_value(Key, Value, Acc, Mapping) when is_map(Value) ->
+    Mapped = map(Value, Mapping),
+    map_property(Key, Mapped, Acc, Mapping);
+map_value(Key, Value, Acc, Mapping) when is_list(Value) ->
+    %% Map any value that is itself a map.
+    Mapped = lists:filtermap(
+        fun(V) when is_map(V) ->
+            {true, map(V, Mapping)};
+        (<<>>) ->
+            false;
+        ([]) ->
+            false;
+        (V) ->
+            {true, V}
+        end,
+        Value
+    ),
+    map_property(Key, Mapped, Acc, Mapping);
+map_value(Key, Value, Acc, Mapping) ->
+    map_property(Key, Value, Acc, Mapping).
+
 map_property(_, [<<"0000">>], Acc, _Mapping) ->
-    %% Incorrect dates
+    %% Incorrect date
+    Acc;
+map_property(_, [], Acc, _Mapping) ->
     Acc;
 map_property(_, [<<>>], Acc, _Mapping) ->
-    %% Empty dates
     Acc;
 map_property(_, <<>>, Acc, _Mapping) ->
     Acc;
 map_property(Key, [SingleValue], Acc, Mapping) ->
-    ?DEBUG({single, Key, SingleValue}),
     map_property(Key, SingleValue, Acc, Mapping);
-%%map_property(Key, Value, Acc, Mapping) when is_map(Value) ->
-%%    MappedValue = map(Value, Mapping),
-%%    Acc#{Key => MappedValue};
-%%map_property(Key, Value, Acc, Mapping) when is_list(Value) ->
-%%    %% Map any value that is itself a map.
-%%    Acc#{Key => lists:filtermap(
-%%        fun(V) when is_map(V) ->
-%%            {true, map(V, Mapping)};
-%%        (<<>>) ->
-%%            false;
-%%        (V) ->
-%%            {true, V}
-%%        end,
-%%        Value
-%%    )};
 map_property(Key, Value, Acc, Mapping) ->
     Mapping:map_property(Key, Value, Acc).
