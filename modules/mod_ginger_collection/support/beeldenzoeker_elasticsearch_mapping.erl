52362e32025ddac0e1fddf2cdd2b6cfc9fd31154
David de Boer <david@ddeboer.nl>
2017-03-29 14:54:32 +0200

    [collection] Update for Elasticsearch mapping upgrades
    See https://github.com/driebit/mod_elasticsearch/pull/11

--- modules/mod_ginger_collection/support/beeldenzoeker_elasticsearch_mapping.erl
+++ modules/mod_ginger_collection/support/beeldenzoeker_elasticsearch_mapping.erl
@@ -6,23 +6,46 @@
 
 %% @doc Default mappings are based on linked data predicates.
 default_mapping() ->
-    #{<<"properties">> => #{
-        <<"dcterms:created">> => #{
-            <<"type">> => <<"date">>
+    Mapping = #{
+        <<"properties">> => #{
+            <<"acquisition.date">> => #{
+                <<"type">> => <<"date">>
+            },
+            <<"dcterms:created">> => #{
+                <<"type">> => <<"date">>
+            },
+            <<"dbo:creationYear">> => #{
+                <<"type">> => <<"date">>
+            },
+            <<"dbo:productionStartYear">> => #{
+                <<"type">> => <<"date">>
+            },
+            <<"dbo:productionEndDate">> => #{
+                <<"type">> => <<"date">>
+            },
+            <<"dbo:productionEndYear">> => #{
+                <<"type">> => <<"date">>
+            },
+            <<"dcterms:creator">> => #{
+                <<"type">> => <<"nested">>,
+                <<"properties">> => #{
+                    <<"dbpedia-owl:birthDate">> => #{
+                        <<"type">> => <<"date">>
+                    },
+                    <<"dbpedia-owl:deathDate">> => #{
+                        <<"type">> => <<"date">>
+                    }
+                }
+            }
         },
-        <<"dcterms:date">> => #{
-            <<"type">> => <<"date">>
-        },
-        <<"dbo:creationYear">> => #{
-            <<"type">> => <<"date">>
-        },
-        <<"dbo:productionStartYear">> => #{
-            <<"type">> => <<"date">>
-        },
-        <<"dbo:productionEndDate">> => #{
-            <<"type">> => <<"date">>
-        },
-        <<"dbo:productionEndYear">> => #{
-            <<"type">> => <<"date">>
-        }
-    }}.
+        <<"dynamic_templates">> => [
+            #{<<"date">> => #{
+                <<"match">> => <<"dcterms:date">>,
+                <<"mapping">> => #{
+                    <<"type">> => <<"date">>
+                }
+            }}
+        ]
+    },
+    
+    {elasticsearch_mapping:hash(Mapping), Mapping}.
