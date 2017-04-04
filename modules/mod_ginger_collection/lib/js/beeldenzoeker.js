343ae55999528f2c5b58f44b1e1a5173b8a07986
David de Boer <david@ddeboer.nl>
2017-04-04 09:13:27 +0200

    [collection] Remove old moreresults
    This was replaced with 42e2c8bc4b747f5690e69fee1be45e0c02f482ff.

--- modules/mod_ginger_collection/lib/js/beeldenzoeker.js
+++ modules/mod_ginger_collection/lib/js/beeldenzoeker.js
@@ -1,15 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-	// Carousel
-	$('.list-carousel').slick({
-		slidesToShow: 3,
-		slidesToScroll: 1,
+    // Carousel
+    $('.list-carousel').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
         variableWidth: true
-	});
+    });
 
-	$(".fancybox").fancybox();
+    $(".fancybox").fancybox();
 
-	$(".masthead__zoom").fancybox({
+    $(".masthead__zoom").fancybox({
         'openEffect': "fade",
         'margin': 20,
         'autoCenter': true,
@@ -18,7 +18,7 @@ $(document).ready(function(){
         "padding": 0,
         'type': 'image',
         'tpl': {
-                error: '<p class="fancybox-error"></p>'
+            error: '<p class="fancybox-error"></p>'
         },
         'helpers': {
             'title': {
@@ -29,40 +29,23 @@ $(document).ready(function(){
                     'background': 'rgba(76, 76, 76, 0.95)'
                 },
                 locked: false
-            },
+            }
         },
-        beforeLoad : function() {
-	        /* You can use callbacks to customize or disable title */
-	        this.title = false; 
-	    },
-        afterShow: function() {
+        beforeLoad: function () {
+            /* You can use callbacks to customize or disable title */
+            this.title = false;
+        },
+        afterShow: function () {
             $('.fancybox-inner').prepend('<div class="zoom-help"></div>');
 
             $('.fancybox-image')
-            .wrap('<span style="display:inline-block"></span>')
-            .css('display', 'block')
-            .parent()
-            .zoom({
-                magnify: 1.25,
-                on:'click'
-            });
+                .wrap('<span style="display:inline-block"></span>')
+                .css('display', 'block')
+                .parent()
+                .zoom({
+                    magnify: 1.25,
+                    on: 'click'
+                });
         }
     });
-
-    if ($('#more-results').length > 0 ){
-        homeInfinite();
-    }
 });
-
-
-function homeInfinite() {
-    $(window).scroll(function(){
-        var buttonMore = $('#more-results'),
-            scrollOffset = $(window).scrollTop(),
-            windowHeight = $(window).height();
-
-         if ((scrollOffset + windowHeight) >= buttonMore.offset().top ){
-            z_event("moreresults");
-         }
-    });
-}
