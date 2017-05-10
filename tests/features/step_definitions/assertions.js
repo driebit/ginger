5a78f0159944a65c6d78ff3f8dafa384e224d715
David de Boer <david@ddeboer.nl>
2017-05-10 08:59:14 +0200

    [tests] Add visibility and count assertions

--- tests/features/step_definitions/assertions.js
+++ tests/features/step_definitions/assertions.js
@@ -10,7 +10,20 @@ defineSupportCode(({Then}) => {
         return client.assert.containsText(element, text);
     });
 
+    Then(/^I should not see "([^"]*)"$/, (element) => {
+        return client.useCss().waitForElementNotVisible(element);
+    });
+
     Then(/^I should see "([^"]*)"$/, (element) => {
         return client.useCss().waitForElementVisible(element);
     });
+
+    Then(/^there should be (\d+) "([^"]*)" elements$/, (number, element) => {
+        // Replace this hard wait with a custom waitForElementCount command.
+        client.pause(500);
+
+        return client.elements('css selector', element, (result) => {
+            client.assert.equal(result.value.length, number);
+        });
+    })
 });
