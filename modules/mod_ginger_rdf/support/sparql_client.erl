02c6c4bcc58eb49066e60decf666e091e24776ad
David de Boer <david@ddeboer.nl>
2017-06-26 18:02:43 +0200

    [rdf] Add premature SPARQL client

---
+++ modules/mod_ginger_rdf/support/sparql_client.erl
@@ -0,0 +1,32 @@
+-module(sparql_client).
+
+-export([
+    describe/2
+]).
+
+-include_lib("zotonic.hrl").
+
+describe(Endpoint, <<"https://", _/binary>> = Uri) ->
+    describe(Endpoint, <<"<", Uri/binary, ">">>);
+describe(Endpoint, <<"http://", _/binary>> = Uri) ->
+    describe(Endpoint, <<"<", Uri/binary, ">">>);
+describe(Endpoint, Clause) ->
+    query(Endpoint, z_convert:to_binary(z_url:url_encode(<<"DESCRIBE ", Clause/binary>>))).
+    
+query(Endpoint, Query) ->
+    Url = <<Endpoint/binary, "?query=", Query/binary>>,
+    case ginger_http_client:get(Url, headers()) of
+        undefined ->
+            undefined;
+        Map ->
+            decode(Map)
+    end.
+
+%% Graph containing a
+decode(#{<<"@graph">> := _} = Data) ->
+    ginger_json_ld:deserialize(Data).
+
+headers() ->
+    [
+        {"Accept", "application/ld+json"}
+    ].
