bca77ec960bfb5ee167e0538d23a4ec8549e2b80
rl-king <hello@rubenlieking.com>
2017-09-12 11:28:45 +0200

    [skel] Improve Gulp setup (#326)
    * Livereload scss, js, and tpl files
    * Transpiles javascript to the last two browser versions
    * Minify javascript

--- skel/gulpfile.js
+++ skel/gulpfile.js
@@ -1,44 +1,83 @@
-'use strict';
+const gulp = require('gulp');
 
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    postcss = require('gulp-postcss'),
-    autoprefixer = require('autoprefixer'),
-    lost = require('lost'),
-    globbing = require('gulp-css-globbing'),
-    livereload = require('gulp-livereload');
+// Scss
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const lost = require('lost');
+const globbing = require('gulp-css-globbing');
 
-var paths = {
-    modulesSrc: [
-        '../../modules/mod_ginger_foundation/lib/css/src',
-    ],
-    cssSource: 'lib/css/src/',
-    cssDestination: 'lib/css/site/'
+// Javascript
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+
+// Prevents the pipe/stream from breaking on error
+const plumber = require('gulp-plumber');
+
+// Livereload
+const livereload = require('gulp-livereload');
+
+// Locations of scss, javascript and template files
+// Add scss file paths to de the modulesSrc array to trigger scss proccessing on file change
+// e.g. '../../modules/mod_ginger_foundation/lib/css/src'
+const paths = {
+    modulesSrc: [],
+    scssSrc: 'lib/css/src',
+    scssDest: 'lib/css/site',
+    javascriptSrc: 'lib/js/src',
+    javascriptDest: 'lib/js/dist',
+    templateSrc: 'templates'
 };
 
-gulp.task('sass', function () {
-    gulp.src(paths.cssSource + 'screen.scss')
+// Reload page when .tpl file changes
+gulp.task('tpl', () => gulp.src('/').pipe(livereload()));
+
+// Process, minify and prefix scss files
+gulp.task('sass', () => {
+    gulp.src(`${paths.scssSrc}/screen.scss`)
+        .pipe(plumber())
         .pipe(globbing({ extensions: ['.scss'] }))
-        .pipe(sass({outputStyle : 'compressed'}))
+        .pipe(sass({ outputStyle: 'compressed' }))
         .on('error', sass.logError)
         .pipe(postcss([
             lost(),
             autoprefixer('last 2 versions', 'ie > 7')
         ]))
-        .pipe(gulp.dest(paths.cssDestination))
+        .pipe(gulp.dest(paths.scssDest))
         .pipe(livereload());
 });
 
-gulp.task('sass:watch', function () {
-    var modules = paths.modulesSrc.map(function(path) {
-        return path + '/**/*.scss';
-    });
-
-    var watchPaths = [
-        paths.cssSource + '/**/*.scss'
-    ].concat(modules);
-
-    gulp.watch(watchPaths, ['sass']);
+// Transpiles ES6 -> minifies -> renames javaScript
+gulp.task('js', () => {
+    gulp.src(`${paths.javascriptSrc}/**/*.js`)
+        .pipe(plumber())
+        .pipe(babel({
+            'presets': [
+                ['env', {
+                    'targets': {
+                        'browsers': ['last 2 versions']
+                    }
+                }]
+            ]
+        }))
+        .pipe(uglify())
+        .pipe(rename(path => path.basename += '.min'))
+        .pipe(gulp.dest(paths.javascriptDest))
+        .pipe(livereload())
 });
 
-gulp.task('default', ['sass', 'sass:watch']);
+// Init livereload server and watch scss, javascript and template files for changes
+gulp.task('watch', () => {
+    livereload.listen();
+
+    const scssCombinedPaths = paths.modulesSrc
+        .map(x => `${x}/**/*.scss`)
+        .concat(`${paths.scssSrc}/**/*.scss`);
+
+    gulp.watch(scssCombinedPaths, ['sass']);
+    gulp.watch(`${paths.templateSrc}/**/*.tpl`, ['tpl']);
+    gulp.watch(`${paths.javascriptSrc}/**/*.js`, ['js']);
+});
+
+gulp.task('default', ['sass', 'js', 'watch']);
