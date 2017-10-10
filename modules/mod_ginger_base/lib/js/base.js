43afc95f5a8ce67daeeebe446bebec13d1c3cc67
rl-king <hello@rubenlieking.com>
2017-10-10 23:51:43 +0200

    Remove unused code + global eventListener

--- modules/mod_ginger_base/lib/js/base.js
+++ modules/mod_ginger_base/lib/js/base.js
@@ -1,99 +1,60 @@
-(function($) {
-    'use strict';
+$.widget("ui.base", {
 
-    $.widget("ui.base", {
-
-        _init: function() {
-            this.init();
-        },
-
-        init: function() {
-            var me = this,
-                element = me.element;
-
-            //fancybox
-            $(".lightbox").fancybox({
-                'openEffect': "fade",
-                'autoSize': true,
-                'autoScale': true,
-                'margin': 50,
-                'autoCenter': true,
-                'autoResize': true,
-                'fitToView': true,
-                'tpl': {
-                        error    : '<p class="fancybox-error"></p>'
-                },
-                'helpers': {
-                    'overlay': {
-                        'css': {
-                            'background': 'rgba(246, 246, 246, 0.75)'
-                        },
-                        locked: false
+    _init: function() {
+        $(".lightbox").fancybox({
+            'openEffect': "fade",
+            'autoSize': true,
+            'autoScale': true,
+            'margin': 50,
+            'autoCenter': true,
+            'autoResize': true,
+            'fitToView': true,
+            'tpl': {
+                    error    : '<p class="fancybox-error"></p>'
+            },
+            'helpers': {
+                'overlay': {
+                    'css': {
+                        'background': 'rgba(246, 246, 246, 0.75)'
                     },
-                    media: {},
-                    'title': {
-                        type: 'inside'
-                    }
+                    locked: false
                 },
-                afterShow: function() {
-
-                    var el = $(this.element),
-                        fbInner = $(".fancybox-inner");
-
-                    if (el.hasClass('default-video-player')) {
-                        var url = el.data('video-url'),
-                            height = el.data('video-height'),
-                            width = el.data('video-width'),
-                            videoHTML = '<video width="' + width +'" height="' + height + '" controls>' +
-                              '<source src="' + url + '">' +
-                              '</video>';
-
-                        $('.fancybox-inner').hide();
-                        $(".fancybox-inner").html(videoHTML);
-                        $('.fancybox-inner').show();
-                        $.fancybox.update();
-                    }
+                media: {},
+                'title': {
+                    type: 'inside'
                 }
-            });
+            },
+            afterShow: function() {
 
-            $(document).on('click', $.proxy(me._documentClick, me));
-            $(document).on('touchend', $.proxy(me._documentClick, me));
-            $(document).on('keyup', $.proxy(me._documentKeyUp, me));
+                var el = $(this.element),
+                    fbInner = $(".fancybox-inner");
 
-        },
+                if (el.hasClass('default-video-player')) {
+                    var url = el.data('video-url'),
+                        height = el.data('video-height'),
+                        width = el.data('video-width'),
+                        videoHTML = '<video width="' + width +'" height="' + height + '" controls>' +
+                            '<source src="' + url + '">' +
+                            '</video>';
 
-        _documentClick: function(event) {
-
-            var me = this;
-
-            if (!$(event.target).closest('form[role="search"]').length) {
-
-                var searchWidgets = $(':ui-search_suggestions'),
-                    isOpen = false;
-
-                $.each(searchWidgets, function(i, widget) {
-
-                    var inst = $(widget).data('ui-search_suggestions');
-                    if (inst && inst.isVisible()) {
-                        isOpen = true;
-                    }
-                });
-
-                if (isOpen) {
-                    $(document).trigger('search:close');
+                    $('.fancybox-inner').hide();
+                    $(".fancybox-inner").html(videoHTML);
+                    $('.fancybox-inner').show();
+                    $.fancybox.update();
                 }
             }
+        });
 
-        },
+        this._addEventListeners();
+    },
 
-        _documentKeyUp: function(event) {
+    _addEventListeners: function() {
+        $(document).on('keyup', this._documentKeyUp.bind(this));
+    },
 
-            if (event.keyCode == 27) {
-                $(document).trigger('search:close');
-                $(document).trigger('menu:close');
-            }
+    _documentKeyUp: function(event) {
+        if (event.keyCode == 27) {
+            $(document).trigger('menu:close');
         }
-
-
-    });
-})(jQuery);
+    }
+});
