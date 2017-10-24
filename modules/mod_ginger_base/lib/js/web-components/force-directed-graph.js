6f7b8f485fd3cb581981b568c69d37699b82a922
rl-king <hello@rubenlieking.com>
2017-10-24 19:55:54 +0200

    Add labels to FDG

--- modules/mod_ginger_base/lib/js/web-components/force-directed-graph.js
+++ modules/mod_ginger_base/lib/js/web-components/force-directed-graph.js
@@ -1,7 +1,10 @@
 // Create a class for the element
 class ForceDirectedGraph extends HTMLElement {
+
+    // Before the element is attached to the DOM
     constructor() {
-        // Always call super first in constructor
+        // If you define a ctor, always call super() first!
+        // This is specific to CE and required by the spec.
         super();
 
         // Create a shadow root
@@ -18,104 +21,94 @@ class ForceDirectedGraph extends HTMLElement {
         // Add the svg element to the shadow root.
         this.svg = d3.select(shadow).append("svg");
 
+        // Set svg dimension attributes
         this.svg
             .attr("width", this.width)
             .attr("height", this.height);
-    }
 
-    connectedCallback() {
-
-        const color = d3.scaleOrdinal(d3.schemeCategory20);
-
-        const simulation = d3.forceSimulation()
-            .force("link", d3.forceLink().id(function (d) {
-                return d.id;
-            }))
+        this.simulation = d3.forceSimulation()
+            .force("link", d3.forceLink().id(d => d.id))
             .force("charge", d3.forceManyBody())
             .force("center", d3.forceCenter(this.width / 2, this.height / 2));
 
-        d3.json(`${this.endpoint}?id=${this.resourceId}`, function (error, graph) {
-            if (error) throw error;
+        this.color = d3.scaleOrdinal(d3.schemeCategory20);
+    }
 
-            console.log(graph)
+    // After the element is attached to the DOM
+    connectedCallback() {
 
-            const link = this.svg.append("g")
-                .attr("class", "links")
-                .selectAll("line")
+        // Get the graph nodes and edges from the endoint and int the FDG
+        d3.json(`${this.endpoint}?id=${this.resourceId}`, (error, graph) => {
+            if (error) {
+                console.log("Something went wrong while getting the data", error);
+                return;
+            }
+
+            this.link = this.svg
+                .selectAll(".line")
                 .data(graph.links)
                 .enter().append("line")
                 .attr("stroke", "#ccc")
-                .attr("stroke-width", function (d) {
-                    return Math.sqrt(d.value);
-                });
+                .attr("stroke-width", "1px");
 
-            const node = this.svg.append("g")
-                .attr("class", "nodes")
-                .selectAll("circle")
+            this.nodes = this.svg
+                .selectAll(".node")
                 .data(graph.nodes)
-                .enter().append("circle")
-                .attr("r", 6)
-                .attr("fill", function (d) {
-                    return color(d.group);
-                })
+                .enter().append("g")
                 .call(d3.drag()
-                    .on("start", dragstarted)
-                    .on("drag", dragged)
-                    .on("end", dragended));
+                    .on("start", this.dragstarted.bind(this))
+                    .on("drag", this.dragged.bind(this))
+                    .on("end", this.dragended.bind(this)));
 
-            node.append("title")
-                .text(function (d) {
-                    return d.title;
-                });
+            this.labels = this.nodes
+                .append("circle")
+                .attr("r", 6)
+                .attr("fill", "red")
 
-            simulation
+            this.labels = this.nodes
+                .append("text")
+                .attr("dx", 12)
+                .attr("dy", ".35em")
+                .text(d => d.title);
+
+            this.simulation
                 .nodes(graph.nodes)
-                .on("tick", ticked);
+                .on("tick", this.tick.bind(this));
 
-            simulation.force("link")
+            this.simulation.force("link")
                 .links(graph.links);
 
-            function ticked() {
-                link
-                    .attr("x1", function (d) {
-                        return d.source.x;
-                    })
-                    .attr("y1", function (d) {
-                        return d.source.y;
-                    })
-                    .attr("x2", function (d) {
-                        return d.target.x;
-                    })
-                    .attr("y2", function (d) {
-                        return d.target.y;
-                    });
+        });
+    }
 
-                node
-                    .attr("cx", function (d) {
-                        return d.x;
-                    })
-                    .attr("cy", function (d) {
-                        return d.y;
-                    });
-            }
-        }.bind(this));
+    // Tick reffers to the browsers Animation Frame
+    tick() {
+        this.link
+            .attr("x1", d => d.source.x)
+            .attr("y1", d => d.source.y)
+            .attr("x2", d => d.target.x)
+            .attr("y2", d => d.target.y);
 
-        function dragstarted(d) {
-            if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-            d.fx = d.x;
-            d.fy = d.y;
-        }
+        this.nodes
+            .attr("transform", d =>
+                "translate(" + d.x + "," + d.y + ")");
+    }
 
-        function dragged(d) {
-            d.fx = d3.event.x;
-            d.fy = d3.event.y;
-        }
+    dragstarted(d) {
+        if (!d3.event.active) this.simulation.alphaTarget(0.3).restart();
+        d.fx = d.x;
+        d.fy = d.y;
+    }
 
-        function dragended(d) {
-            if (!d3.event.active) simulation.alphaTarget(0);
-            d.fx = null;
-            d.fy = null;
-        }
+    dragged(d) {
+        d.fx = d3.event.x;
+        d.fy = d3.event.y;
+    }
+
+    dragended(d) {
+        if (!d3.event.active) this.simulation.alphaTarget(0);
+        d.fx = null;
+        d.fy = null;
     }
 }
 
