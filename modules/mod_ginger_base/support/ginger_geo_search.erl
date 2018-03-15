3708cf9babc22baea02c63c900f33ceb36b49897
David de Boer <david@ddeboer.nl>
2018-03-15 09:24:57 +0100

    Keep Ginger search arguments.
    * Slightly shorten SQL query using ^2.

--- modules/mod_ginger_base/support/ginger_geo_search.erl
+++ modules/mod_ginger_base/support/ginger_geo_search.erl
@@ -20,11 +20,48 @@ search_query(#search_query{search={ginger_geo, Args}}, Context) ->
 
 %% @doc Similar to z_geo_search:search_query/2 but result set includes locations and category
 search_query(#search_query{search = {ginger_geo_nearby, Args}}, Context) ->
-    Sql = #search_sql{select = Select} = geo_search(Args, Context),
-    Sql#search_sql{
-        select = Select ++ ", r.pivot_location_lat, r.pivot_location_lng, r.category_id",
-        limit = "limit 5000"
-    }.
+    BaseArgs = lists:filter(
+        fun({Key, _}) ->
+            not lists:member(Key, [distance, lat, lng])
+        end,
+        Args
+    ),
+    BaseSearch = search_query(#search_query{search = {ginger_geo, BaseArgs}}, Context),
+    with_geo_nearby(BaseSearch, Args, Context).
+
+%% @doc Extend an SQL query with geo nearby search based on Args.
+-spec with_geo_nearby(#search_sql{}, proplists:proplist(), z:context()) -> #search_sql{}.
+with_geo_nearby(#search_sql{} = SearchSql, Args, Context) ->
+    GeoArgs = geo_args(Args, Context),
+    {Placeholders, SearchWithGeoArgs} = add_args(GeoArgs, SearchSql),
+
+    %% Rebuild WHERE and ORDER BY because argument positions have shifted.
+    GeoWhere = arg(1, Placeholders) ++ " < pivot_location_lat AND " ++ arg(2, Placeholders) ++ "< pivot_location_lng "
+            ++ " AND pivot_location_lat < " ++ arg(3, Placeholders) ++ " AND pivot_location_lng < " ++ arg(4, Placeholders),
+    GeoOrder = "(pivot_location_lat-" ++ arg(5, Placeholders) ++ ")^2"
+        ++ " + (pivot_location_lng-" ++ arg(6, Placeholders) ++ ")^2",
+    
+    %% Add geo WHERE to the Ginger search query.
+    and_where(
+        SearchWithGeoArgs#search_sql{order = GeoOrder},
+        GeoWhere
+    ).
+
+%% @doc Get positioned argument from list of arguments.
+-spec arg(pos_integer(), [string()]) -> string().
+arg(Position, Args) ->
+    lists:nth(Position, Args).
+
+%% @doc Add arguments to an existing #search_sql{} query.
+-spec add_args([any()], #search_sql{}) -> {[string()], #search_sql{}}.
+add_args(AddArguments, #search_sql{args = Arguments} = Search) ->
+    Positions = lists:map(
+        fun(Index) ->
+            "$" ++ integer_to_list(Index)
+        end,
+        lists:seq(length(Arguments) + 1, length(AddArguments) + 1)
+    ),
+    {Positions, Search#search_sql{args = Arguments ++ AddArguments}}.
 
 %% @doc Adds a custom where argument to SQL query (using the and operator)
 and_where(#search_sql{} = SearchSql, WhereStr) ->
@@ -38,6 +75,11 @@ and_where(#search_sql{} = SearchSql, WhereStr) ->
         where = Where
     }.
 
--spec geo_search(proplists:proplist(), z:context()) -> #search_sql{}.
-geo_search(Args, Context) ->
-    z_geo_search:search_query(#search_query{search = {geo_nearby, Args}}, Context).
+%% @doc Get geo SQL arguments through geo search.
+-spec geo_args(proplists:proplist(), z:context()) -> [float()].
+geo_args(Args, Context) ->
+    #search_sql{args = GeoArgs} = z_geo_search:search_query(
+        #search_query{search = {geo_nearby, Args}},
+        Context
+    ),
+    GeoArgs.
