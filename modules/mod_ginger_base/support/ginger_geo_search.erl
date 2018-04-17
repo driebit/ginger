d940db34817dbe36508f3a88eb91ccdfeb5d9e5a
David de Boer <david@ddeboer.nl>
2018-04-17 07:57:11 +0200

    [base] Fix ginger_geo_nearby with negative lat/lng (#371)
    * [base] Fix ginger_geo_nearby with negative lat/lng
    
    * Re-use Zotonic's geo search instead of duplicating the code.
    
    * Keep Ginger search arguments.
    
    * Slightly shorten SQL query using ^2.

--- modules/mod_ginger_base/support/ginger_geo_search.erl
+++ modules/mod_ginger_base/support/ginger_geo_search.erl
@@ -19,45 +19,49 @@ search_query(#search_query{search={ginger_geo, Args}}, Context) ->
     };
 
 %% @doc Similar to z_geo_search:search_query/2 but result set includes locations and category
-search_query(#search_query{search={ginger_geo_nearby, Args}}, Context) ->
-
+search_query(#search_query{search = {ginger_geo_nearby, Args}}, Context) ->
     BaseArgs = lists:filter(
         fun({Key, _}) ->
             not lists:member(Key, [distance, lat, lng])
         end,
         Args
     ),
-    BaseSearch = ginger_geo_search:search_query(#search_query{search={ginger_geo, BaseArgs}}, Context),
+    BaseSearch = search_query(#search_query{search = {ginger_geo, BaseArgs}}, Context),
+    with_geo_nearby(BaseSearch, Args, Context).
 
-    Distance = z_convert:to_float(proplists:get_value(distance, Args, 10)),
-    {Lat, Lng} = z_geo_search:get_query_center(Args, Context),
-    {LatMin, LngMin, LatMax, LngMax} = z_geo_support:get_lat_lng_bounds(Lat, Lng, Distance),
+%% @doc Extend an SQL query with geo nearby search based on Args.
+-spec with_geo_nearby(#search_sql{}, proplists:proplist(), z:context()) -> #search_sql{}.
+with_geo_nearby(#search_sql{} = SearchSql, Args, Context) ->
+    GeoArgs = geo_args(Args, Context),
+    {Placeholders, SearchWithGeoArgs} = add_args(GeoArgs, SearchSql),
 
-    WhereStr = string:join(
-        [
-            z_convert:to_list(LatMin),
-            "< rsc.pivot_location_lat AND",
-            z_convert:to_list(LngMin),
-            "< rsc.pivot_location_lng AND",
-            "rsc.pivot_location_lat <",
-            z_convert:to_list(LatMax),
-            "AND rsc.pivot_location_lng <",
-            z_convert:to_list(LngMax)
-        ],
-        " "
+    %% Rebuild WHERE and ORDER BY because argument positions have shifted.
+    GeoWhere = arg(1, Placeholders) ++ " < pivot_location_lat AND " ++ arg(2, Placeholders) ++ "< pivot_location_lng "
+            ++ " AND pivot_location_lat < " ++ arg(3, Placeholders) ++ " AND pivot_location_lng < " ++ arg(4, Placeholders),
+    GeoOrder = "(pivot_location_lat-" ++ arg(5, Placeholders) ++ ")^2"
+        ++ " + (pivot_location_lng-" ++ arg(6, Placeholders) ++ ")^2",
+    
+    %% Add geo WHERE to the Ginger search query.
+    and_where(
+        SearchWithGeoArgs#search_sql{order = GeoOrder},
+        GeoWhere
+    ).
+
+%% @doc Get positioned argument from list of arguments.
+-spec arg(pos_integer(), [string()]) -> string().
+arg(Position, Args) ->
+    lists:nth(Position, Args).
+
+%% @doc Add arguments to an existing #search_sql{} query.
+-spec add_args([any()], #search_sql{}) -> {[string()], #search_sql{}}.
+add_args(AddArguments, #search_sql{args = Arguments} = Search) ->
+    Positions = lists:map(
+        fun(Index) ->
+            "$" ++ integer_to_list(Index)
+        end,
+        lists:seq(length(Arguments) + 1, length(AddArguments) + 1)
     ),
-    SearchSql = and_where(BaseSearch, WhereStr),
-
-    LatStr = z_convert:to_list(Lat),
-    LngStr = z_convert:to_list(Lng),
-    OrderStr = string:join(
-        [
-            "(pivot_location_lat-", LatStr, ")*(pivot_location_lat-", LatStr,
-            ") + (pivot_location_lng-", LngStr, ")*(pivot_location_lng-", LngStr, ")"
-        ],
-        ""
-    ),
-    SearchSql#search_sql{order=OrderStr}.
+    {Positions, Search#search_sql{args = Arguments ++ AddArguments}}.
 
 %% @doc Adds a custom where argument to SQL query (using the and operator)
 and_where(#search_sql{} = SearchSql, WhereStr) ->
@@ -71,3 +75,11 @@ and_where(#search_sql{} = SearchSql, WhereStr) ->
         where = Where
     }.
 
+%% @doc Get geo SQL arguments through geo search.
+-spec geo_args(proplists:proplist(), z:context()) -> [float()].
+geo_args(Args, Context) ->
+    #search_sql{args = GeoArgs} = z_geo_search:search_query(
+        #search_query{search = {geo_nearby, Args}},
+        Context
+    ),
+    GeoArgs.
