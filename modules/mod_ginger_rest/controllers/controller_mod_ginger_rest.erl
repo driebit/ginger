649214a44eb6394220235e94b1ec319276ecc576
Michel Rijnders <michel@driebit.nl>
2018-06-08 17:35:54 +0200

    Add “mod_ginger_rest” module

---
+++ modules/mod_ginger_rest/controllers/controller_mod_ginger_rest.erl
@@ -0,0 +1,22 @@
+-module(controller_mod_ginger_rest).
+
+-export([ init/1
+        , content_types_provided/2
+        , to_json/2
+        ]).
+
+
+-include("controller_webmachine_helper.hrl").
+-include("zotonic.hrl").
+
+init(_Config) ->
+    {ok, undefined}.
+
+content_types_provided(Req, State) ->
+    {[{"application/json", to_json}], Req, State}.
+
+to_json(Req, State) ->
+    Context = z_context:new(Req, ?MODULE),
+    Resources = z_search:query_([{filter, ["is_published", true]}], Context),
+    Json = jsx:encode(Resources),
+    {Json, Req, State}.
---
+++ modules/mod_ginger_rest/mod_ginger_rest.erl
@@ -0,0 +1,6 @@
+-module(mod_ginger_rest).
+-author("Driebit <info@driebit.nl>").
+
+-mod_title("Ginger REST module").
+-mod_description("Provides a REST API").
+-mod_prio(100).
