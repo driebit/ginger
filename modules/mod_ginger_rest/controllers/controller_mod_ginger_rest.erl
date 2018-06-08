15e815abd0ae16c1c9c36032414634726903be6d
Michel Rijnders <michel@driebit.nl>
2018-06-08 17:46:10 +0200

    Add TODOs

--- modules/mod_ginger_rest/controllers/controller_mod_ginger_rest.erl
+++ modules/mod_ginger_rest/controllers/controller_mod_ginger_rest.erl
@@ -17,6 +17,11 @@ content_types_provided(Req, State) ->
 
 to_json(Req, State) ->
     Context = z_context:new(Req, ?MODULE),
+    %% TODO:
+    %% - use `ginger_search` instead of `z_search`
+    %% - incorporate access control
+    %% - load and convert resources (instead of returning IDs)
+    %% - process query parameters
     Resources = z_search:query_([{filter, ["is_published", true]}], Context),
     Json = jsx:encode(Resources),
     {Json, Req, State}.
