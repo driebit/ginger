1814c7600bb705412d097f04a21c94cebac086ea
Michel Rijnders <michel@driebit.nl>
2018-06-11 09:30:09 +0200

    Use “ginger_search” for query args

--- modules/mod_ginger_rest/controllers/controller_mod_ginger_rest.erl
+++ modules/mod_ginger_rest/controllers/controller_mod_ginger_rest.erl
@@ -23,10 +23,12 @@ content_types_provided(Req, _) ->
 
 to_json(Req, State = #state{context = Context}) ->
     %% TODO:
-    %% - use `ginger_search` instead of `z_search`
     %% - incorporate access control
     %% - load and convert resources (instead of returning IDs)
     %% - process query parameters
-    Resources = z_search:query_([{filter, ["is_published", true]}], Context),
+    Args = ginger_search:query_arguments(
+             [{cat_exclude_defaults, false}, {filter, ["is_published", true]}],
+             Context),
+    Resources = z_search:query_(Args, Context),
     Json = jsx:encode(Resources),
     {Json, Req, State}.
