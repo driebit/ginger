c4d27e710689a7eac97731066131acaecead4302
Michel Rijnders <michel@driebit.nl>
2018-06-11 12:54:48 +0200

    Add `resource_exists/2`

--- modules/mod_ginger_base/controllers/controller_rest.erl
+++ modules/mod_ginger_base/controllers/controller_rest.erl
@@ -2,6 +2,7 @@
 
 -export([
          init/1,
+         resource_exists/2,
          content_types_provided/2,
          to_json/2
         ]).
@@ -14,13 +15,22 @@
 %% we reserve "context" for the way it's used by Zotonic/Ginger.
 -record(state, {context, mode}).
 
+
+%%%-----------------------------------------------------------------------------
+%%% Resource functions
+%%%-----------------------------------------------------------------------------
+
 init(Args) ->
     Mode = proplists:get_value(mode, Args),
     {ok, #state{mode = Mode}}.
 
+resource_exists(Req, State = #state{mode = colletion}) ->
+    {false, Req, State#state{context = context(Req)}};
+resource_exists(Req, State) ->
+    {false, Req, State#state{context = context(Req)}}.
+
 content_types_provided(Req, State) ->
-    Context = z_context:new(Req, ?MODULE),
-    {[{"application/json", to_json}], Req, State#state{context = Context}}.
+    {[{"application/json", to_json}], Req, State}.
 
 to_json(Req, State = #state{context = Context, mode = collection}) ->
     %% TODO:
@@ -33,3 +43,10 @@ to_json(Req, State = #state{context = Context, mode = collection}) ->
     Resources = z_search:query_(Args, Context),
     Json = jsx:encode(Resources),
     {Json, Req, State}.
+
+%%%-----------------------------------------------------------------------------
+%%% Internal functions
+%%%-----------------------------------------------------------------------------
+
+context(Req) ->
+    z_context:new(Req, ?MODULE).
