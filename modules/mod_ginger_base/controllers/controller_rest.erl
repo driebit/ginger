3d754fe6086b4fd5c6585f11843304928e2afe74
Michel Rijnders <michel@driebit.nl>
2018-06-11 14:45:45 +0200

    Remove context from state record

--- modules/mod_ginger_base/controllers/controller_rest.erl
+++ modules/mod_ginger_base/controllers/controller_rest.erl
@@ -7,13 +7,12 @@
          to_json/2
         ]).
 
-
 -include("controller_webmachine_helper.hrl").
 -include("zotonic.hrl").
 
 %% NB: the Webmachine documenation uses "context" where we use "state",
 %% we reserve "context" for the way it's used by Zotonic/Ginger.
--record(state, {context, mode}).
+-record(state, {mode}).
 
 
 %%%-----------------------------------------------------------------------------
@@ -25,18 +24,19 @@ init(Args) ->
     {ok, #state{mode = Mode}}.
 
 resource_exists(Req, State = #state{mode = colletion}) ->
-    {false, Req, State#state{context = context(Req)}};
+    {false, Req, State};
 resource_exists(Req, State) ->
-    {false, Req, State#state{context = context(Req)}}.
+    {false, Req, State}.
 
 content_types_provided(Req, State) ->
     {[{"application/json", to_json}], Req, State}.
 
-to_json(Req, State = #state{context = Context, mode = collection}) ->
+to_json(Req, State = #state{mode = collection}) ->
     %% TODO:
     %% - incorporate access control
     %% - load and convert resources (instead of returning IDs)
     %% - process query parameters
+    Context = z_context:new(Req, ?MODULE),
     Args = ginger_search:query_arguments(
              [{cat_exclude_defaults, false}, {filter, ["is_published", true]}],
              Context),
@@ -47,6 +47,3 @@ to_json(Req, State = #state{context = Context, mode = collection}) ->
 %%%-----------------------------------------------------------------------------
 %%% Internal functions
 %%%-----------------------------------------------------------------------------
-
-context(Req) ->
-    z_context:new(Req, ?MODULE).
