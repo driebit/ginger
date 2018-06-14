226481633c701b51cf0aa4e5cc70167ca6e18872
David de Boer <david@ddeboer.nl>
2018-06-14 08:44:32 +0200

    [activity] Add 'to' (primary audience) for mentions
    * Add to property.
    * Add integration test.
    * Prepare for activity as an opaque type (best practice for records) by
      moving record generation into the model (m_ginger_activity).
    
    Start the module
    
    Add 'to' property and make test pass
    
    Try to raise timeout
    
    Enable logged user assertion
    
    Try notify
    
    Add to_ids to select
    
    Add support for mentions in remarks
    
    Reduce wait time to fix gen_server timeout (5 seconds)
    
    Debug
    
    Try a sleep
    
    Debug more
    
    Raise test timeout
    
    Replace init/1 with module_schema/2, as await does not work with init
    
    See https://github.com/zotonic/zotonic/issues/1916
    
    Raise timeout for local runs

--- modules/mod_ginger_activity/models/m_ginger_activity.erl
+++ modules/mod_ginger_activity/models/m_ginger_activity.erl
@@ -3,18 +3,49 @@
 -behaviour(gen_model).
 
 -export([
-    rsc_count/2,
     m_find_value/3,
     m_to_list/2,
     m_value/2,
-    ensure_target_column/1
+    rsc_count/2,
+    activity/2,
+    target/2,
+    to/2,
+    init/1
 ]).
 
 -include_lib("zotonic.hrl").
+-include_lib("../include/ginger_activity.hrl").
+
+%% @doc Initialize the database.
+-spec init(z:context()) -> ok.
+init(Context) ->
+    ensure_target_column(Context),
+    ensure_to_column(Context).
 
 rsc_count(RscId, Context) ->
     z_db:q1("select count(id) from activity_log where rsc_id = $1;",
             [z_convert:to_integer(RscId)], Context).
+
+%% @doc Create a new activity.
+-spec activity(m_rsc:resource(), z:context()) -> mod_ginger_activity:activity().
+activity(Rsc, Context) ->
+    #ginger_activity{
+        rsc_id = Rsc,
+        time = calendar:universal_time(),
+        user_id = z_acl:user(Context),
+        ip_address = ip_address(Context)
+    }.
+
+%% @doc Address activity to a public primary audience.
+%%      See https://www.w3.org/TR/activitystreams-vocabulary/
+-spec to(mod_ginger_activity:activity(), [m_rsc:resource()]) -> mod_ginger_activity:activity().
+to(#ginger_activity{} = Activity, To) ->
+    Activity#ginger_activity{to = To}.
+
+%% @doc Direct activity to a target.
+-spec target(mod_ginger_activity:activity(), m_rsc:resource()) -> mod_ginger_activity:activity().
+target(#ginger_activity{} = Activity, Target) ->
+    Activity#ginger_activity{target_id = Target}.
 
 % Syntax: m.activity[RscId]
 m_find_value(RscId, #m{value=undefined} = M, _Context) ->
@@ -44,5 +75,27 @@ ensure_target_column(Context) ->
                         on delete cascade
                 ;",
                 Context
-            )
+            ),
+            ok
+    end.
+
+ensure_to_column(Context) ->
+    case lists:member(to_ids, z_db:column_names(activity_log, Context)) of
+        true ->
+            ok;
+        false ->
+            [] = z_db:q("alter table activity_log add column to_ids bytea;", Context),
+            ok
+    end.
+
+ip_address(Context) ->
+    case z_context:get_reqdata(Context) of
+        undefined -> undefined;
+        Value ->
+            case proplists:get_value("x-forwarded-for", wrq:req_headers(Value)) of
+                undefined ->
+                    z_convert:to_binary(wrq:peer(Value));
+                Hosts ->
+                    z_convert:to_binary(string:strip(lists:last(string:tokens(Hosts, ","))))
+            end
     end.
--- modules/mod_ginger_activity/models/m_ginger_activity_inbox.erl
+++ modules/mod_ginger_activity/models/m_ginger_activity_inbox.erl
@@ -5,7 +5,11 @@
     fan_out/3,
     for_user/2,
     delete/2,
-    delete/3
+    delete/3,
+    count_for_user/2,
+    seen_at_for_user/2,
+    update_seen_at_for_user/3,
+    most_recent_at_for_user/2
 ]).
 
 -include_lib("zotonic.hrl").
@@ -15,13 +19,41 @@
 -spec for_user(m_rsc:resource(), z:context()) -> [mod_ginger_activity:activity()].
 for_user(UserId, Context) ->
     Activities = z_db:assoc("
-        select * from activity_inbox
+        select
+            activity_log.id,
+            activity_log.rsc_id,
+            activity_log.time,
+            activity_log.target_id,
+            activity_log.to_ids,
+            activity_log.user_id as user_id
+        from activity_inbox
         left join activity_log on activity_log.id = activity_inbox.activity_id
         where activity_inbox.user_id = $1",
         [UserId],
         Context
     ),
     [ginger_activity(A) || A <- Activities].
+
+-spec count_for_user(m_rsc:resource(), z:context()) -> integer().
+count_for_user(User, Context) ->
+    z_db:q1("select count(user_id) from " ++ table() ++ " where user_id = $1;",
+        [z_convert:to_integer(User)], Context).
+
+-spec seen_at_for_user(m_rsc:resource(), z:context()) -> calendar:datetime() | undefined.
+seen_at_for_user(User, Context) ->
+    m_rsc:p(User, notifications_seen_at, Context).
+
+-spec update_seen_at_for_user(m_rsc:resource(), calendar:datetime(), z:context()) -> ok.
+update_seen_at_for_user(User, DateTime, Context) ->
+    m_rsc:update(User, [ {notifications_seen_at, DateTime} ], Context).
+
+-spec most_recent_at_for_user(m_rsc:resource(), z:context()) -> calendar:datetime() | undefined.
+most_recent_at_for_user(User, Context) ->
+    z_db:q1("
+        select max(activity_log.time) from activity_inbox
+        left join activity_log on activity_log.id = activity_inbox.activity_id
+        where activity_inbox.user_id = $1",
+        [z_convert:to_integer(User)], Context).
 
 %% @doc Delete all activities from the user's stream.
 -spec delete(m_rsc:resource(), z:context()) -> ok.
@@ -93,11 +125,13 @@ insert(ActivityId, UserId, Context) ->
 table() ->
     "activity_inbox".
 
+-spec ginger_activity(proplists:proplist()) -> #ginger_activity{}.
 ginger_activity(Props) ->
     #ginger_activity{
         id = proplists:get_value(id, Props),
         user_id = proplists:get_value(user_id, Props),
         rsc_id = proplists:get_value(rsc_id, Props),
         time = proplists:get_value(time, Props),
-        target_id = proplists:get_value(target_id, Props)
+        target_id = proplists:get_value(target_id, Props),
+        to = proplists:get_value(to_ids, Props)
     }.
