3858b1c1eec0c49fb487a18d8994a8f11cf21efc
Rob van den Bogaard <robvandenbogaard@gmail.com>
2018-06-18 11:31:07 +0200

    [activity] Add last seen at to inbox (#397)
    * Support for storing notifications seen at status for indicator logic
    
    * Fix some confusion in handling the activity_inbox and activity_log data
    
    * Apologies for actually using client-side time..this of course should be the 'trusted' server time for seen_at

--- modules/mod_ginger_activity/services/service_ginger_activity_inbox.erl
+++ modules/mod_ginger_activity/services/service_ginger_activity_inbox.erl
@@ -21,6 +21,13 @@ process_get(_ReqData, Context) ->
     },
     jsx:encode(Json).
 
+%% @doc Record when the inbox items were last seen by the user.
+process_post(#wm_reqdata{method = 'POST'}, Context) ->
+    User = z_acl:user(Context),
+    DateTime = calendar:universal_time(),
+    m_ginger_activity_inbox:update_seen_at_for_user(User, DateTime, Context),
+    <<>>;
+
 %% @doc Delete an item from the user's activity stream inbox.
 process_post(#wm_reqdata{method = 'DELETE'}, Context) ->
     User = z_acl:user(Context),
