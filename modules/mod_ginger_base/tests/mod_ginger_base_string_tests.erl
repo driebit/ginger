4dd9ee11a7cb985a4ee2d99f91d27eff85f7a5b8
rl-king <hello@rubenlieking.com>
2018-06-22 14:22:06 +0200

    Add string tests

--- modules/mod_ginger_base/tests/mod_ginger_base_string_tests.erl
+++ modules/mod_ginger_base/tests/mod_ginger_base_string_tests.erl
@@ -4,10 +4,15 @@
 -include("zotonic.hrl").
 
 
-encode_test_() ->
-    [
-     fun() ->
-             Result = ginger_string:encode("Hello"),
-             ?assertEqual(<<"Hello">>, Result)
-     end
-    ].
+encode_test() ->
+    ?assertEqual(<<"">>, ginger_string:encode("")),
+    ?assertEqual(<<"Hello">>, ginger_string:encode("Hello")),
+    ?assertEqual(<<"ABC">>, ginger_string:encode([65, 66, 67])),
+    ?assertEqual(<<"">>, ginger_string:encode([])),
+    %% ?assertEqual(<<"Hello">>, ginger_string:encode(<<"Hello">>)), <- Should succeed ?
+    ?assertError("***Unexpected value, see error message***", ginger_string:encode([1])),
+    ?assertError("***Unexpected value, see error message***", ginger_string:encode(#{})),
+    ?assertError("***Unexpected value, see error message***", ginger_string:encode(hello)),
+    ?assertError("***Unexpected value, see error message***", ginger_string:encode(true)),
+    ?assertError("***Unexpected value, see error message***", ginger_string:encode(4.2)),
+    ?assertError("***Unexpected value, see error message***", ginger_string:encode(42)).
