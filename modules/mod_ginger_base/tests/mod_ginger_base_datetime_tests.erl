cd14966d7565b83d5c70aafdfe9515976bf50430
Michel Rijnders <michel@driebit.nl>
2018-06-28 16:52:37 +0200

    Clean up tests

--- modules/mod_ginger_base/tests/mod_ginger_base_datetime_tests.erl
+++ modules/mod_ginger_base/tests/mod_ginger_base_datetime_tests.erl
@@ -1,18 +1,33 @@
 -module(mod_ginger_base_datetime_tests).
 
+-include_lib("proper/include/proper.hrl").
 -include_lib("eunit/include/eunit.hrl").
 -include("zotonic.hrl").
 
 encode_test() ->
     ?assertEqual(
-       {{2018, 06, 12}, {12, 48, 08}},
-       ginger_datetime:encode({{2018, 6, 12}, {12, 48, 8}})
-      ),
-    ?assertError("***Unexpected value, see error message***", ginger_datetime:encode([1])),
-    ?assertError("***Unexpected value, see error message***", ginger_datetime:encode(#{})),
-    ?assertError("***Unexpected value, see error message***", ginger_datetime:encode(hello)),
-    ?assertError("***Unexpected value, see error message***", ginger_datetime:encode(true)),
-    ?assertError("***Unexpected value, see error message***", ginger_datetime:encode("Hello")),
-    ?assertError("***Unexpected value, see error message***", ginger_datetime:encode(<<"Hello">>)),
-    ?assertError("***Unexpected value, see error message***", ginger_datetime:encode(4.2)),
-    ?assertError("***Unexpected value, see error message***", ginger_datetime:encode(42)).
+       true,
+       proper:quickcheck(
+         ?FORALL(
+            {D, T},
+            {
+             {integer(1970, 2100), integer(1, 12), integer(1, 31)},
+             {integer(0, 23), integer(0, 59), integer(0, 59)}
+            },
+            ?IMPLIES(
+               calendar:valid_date(D),
+               ginger_datetime:encode({D, T}) == {D, T}
+              )
+           )
+         )
+       ),
+    ?assertError(_, ginger_datetime:encode([1])),
+    ?assertError(_, ginger_datetime:encode(#{})),
+    ?assertError(_, ginger_datetime:encode(hello)),
+    ?assertError(_, ginger_datetime:encode(true)),
+    ?assertError(_, ginger_datetime:encode("Hello")),
+    ?assertError(_, ginger_datetime:encode(<<"Hello">>)),
+    ?assertError(_, ginger_datetime:encode(4.2)),
+    ?assertError(_, ginger_datetime:encode(42)),
+    %% Done
+    ok.
