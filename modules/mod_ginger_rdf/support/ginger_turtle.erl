ac638e670c086a479a05cd7779c88b1248237245
Michel Rijnders <michel@driebit.nl>
2018-06-29 15:59:32 +0200

    Fix serialization problems

--- modules/mod_ginger_rdf/support/ginger_turtle.erl
+++ modules/mod_ginger_rdf/support/ginger_turtle.erl
@@ -3,56 +3,81 @@
 -include("zotonic.hrl").
 -include("../include/rdf.hrl").
 
--export([serialize/1]).
+-export([serialize/1, escape/1, escape_char/1]).
 
-%% @doc Serialize a RDF resource to Turtle
+%% @doc Serialize a RDF resource to Turtle (https://www.w3.org/TR/turtle/)
 -spec serialize(m_rdf:rdf_resource()) -> iodata().
 serialize(#rdf_resource{triples = Triples}) ->
     [triple_to_turtle(T) || T <- Triples].
 
--spec triple_to_turtle(#triple{}) -> binary().
+triple_to_turtle(#triple{subject = S, predicate = P, object = #rdf_resource{} = Rsc}) ->
+    O = blank_node(Rsc),
+    Ts = [triple_to_turtle(T#triple{subject = O}) || T <- Rsc#rdf_resource.triples],
+    [to_line(subject(S), predicate(P), O), Ts];
+triple_to_turtle(#triple{subject = undefined, predicate = P, object = O}) ->
+    to_line(<<"[]">>, predicate(P), object(O));
 triple_to_turtle(#triple{subject = S, predicate = P, object = O}) ->
-    Object = case O of
-                 #rdf_value{value = V} ->
-                     quotes(V);
-                 _ ->
-                     uri(O)
-             end,
-    <<
-      (uri(S))/binary,
-      (space())/binary,
-      (uri(P))/binary,
-      (space())/binary,
-      Object/binary,
-      (full_stop())/binary,
-      (new_line())/binary
-    >>.
+    to_line(subject(S), predicate(P), object(O)).
 
--spec uri(binary()) -> binary().
-uri(U = <<"http://", _/binary>>) ->
-    brackets(U);
-uri(U = <<"https://", _/binary>>) ->
-    brackets(U);
-uri(U) ->
-    %% Prefixed URI
+to_line(S, P, O) ->
+    [S, " ", P, " ", O, ".\n"].
+
+subject(X) ->
+    uri_or_blank_node(X).
+
+predicate(X) ->
+    uri_or_blank_node(X).
+
+uri_or_blank_node(U = <<"http://", _/binary>>) ->
+    [$<, U, $>];
+uri_or_blank_node(U = <<"https://", _/binary>>) ->
+    [$<, U, $>];
+uri_or_blank_node(U) ->
+    %% Prefixed URI or blank node
     U.
 
--spec brackets(binary()) -> binary().
-brackets(B) ->
-    <<"<", B/binary, ">">>.
+object(#rdf_value{value = undefined}) ->
+    [$", $"];
+object(#rdf_value{value = V}) ->
+    [$", literal(V), $"];
+object(O) ->
+    uri_or_blank_node(O).
 
--spec quotes(binary()) -> binary().
-quotes(B) ->
-    <<"\"", B/binary, "\"">>.
+blank_node(Rsc) ->
+    <<"_:", (erlang:integer_to_binary(erlang:phash2(Rsc)))/binary>>.
 
--spec space() -> binary().
-space() ->
-    <<" ">>.
+literal({{Y, Mo, D}, {H, Mn, S}}) ->
+    erlang:list_to_binary(
+      %% zotonic: z_datetime:format/?
+      io_lib:format(
+        "~4.10.0B-~2.10.0B-~2.10.0BT~2.10.0B:~2.10.0B:~2.10.0BZ",
+        [Y, Mo, D, H, Mn, S]
+       )
+     );
+literal(V) when is_binary(V) ->
+    escape(V);
+literal(V) when is_list(V) ->
+    escape(erlang:list_to_binary(V));
+literal(V) ->
+    V.
 
--spec full_stop() -> binary().
-full_stop() ->
-    <<".">>.
-
--spec new_line() -> binary().
-new_line() ->
-   <<"\n">>.
+escape(<<>>) ->
+    <<>>;
+escape(<<$\b, Rest/binary>>) ->
+    <<$\\, $b, (escape(Rest))/binary>>;
+escape(<<$\t, Rest/binary>>) ->
+    <<$\\, $t, (escape(Rest))/binary>>;
+escape(<<$\n, Rest/binary>>) ->
+    <<$\\, $n, (escape(Rest))/binary>>;
+escape(<<$\f, Rest/binary>>) ->
+    <<$\\, $f, (escape(Rest))/binary>>;
+escape(<<$\r, Rest/binary>>) ->
+    <<$\\, $r, (escape(Rest))/binary>>;
+escape(<<$", Rest/binary>>) ->
+    <<$\\, $", (escape(Rest))/binary>>;
+escape(<<$', Rest/binary>>) ->
+    <<$\\, $', (escape(Rest))/binary>>;
+escape(<<$\\, Rest/binary>>) ->
+    <<$\\, $\\, (escape(Rest))/binary>>;
+escape(<<C/utf8, Cs/binary>>) ->
+    <<C/utf8, (escape(Cs))/binary>>.
