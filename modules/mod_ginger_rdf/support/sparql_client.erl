c2d7692a762165cf736fc17ad309e80c3cae8b23
David de Boer <david@ddeboer.nl>
2018-07-05 17:29:34 +0200

    Improve DBPedia query results

--- modules/mod_ginger_rdf/support/sparql_client.erl
+++ modules/mod_ginger_rdf/support/sparql_client.erl
@@ -5,6 +5,8 @@
     describe/2,
     query/2,
     query/3,
+    query_rdf/2,
+    query_rdf/3,
     get_resource/3
 ]).
 
@@ -12,10 +14,12 @@
 -include_lib("../include/rdf.hrl").
 
 -export_type([
-    url/0
+    url/0,
+    query/0
 ]).
 
 -type url() :: binary().
+-type query() :: sparql_query:sparql_query() | binary().
 
 %% @doc Describe a single resource
 -spec describe(url(), url()) -> m_rdf:rdf_resource() | undefined.
@@ -27,13 +31,13 @@ describe(Endpoint, Clause) ->
     query(Endpoint, (<<"DESCRIBE ", Clause/binary>>)).
 
 %% @doc Execute a SPARQL query.
--spec query(url(), binary()) -> binary() | undefined.
+-spec query(url(), query()) -> binary() | undefined.
 query(Endpoint, Query) ->
     query(Endpoint, Query, headers()).
 
 %% @doc Execute a SPARQL query with some HTTP headers.
--spec query(url(), binary(), map()) -> list().
-query(Endpoint, Query, Headers) ->
+-spec query(url(), query(), map()) -> map().
+query(Endpoint, Query, Headers) when is_binary(Query) ->
     lager:debug("sparql_client query on endpoint ~s: ~s", [Endpoint, Query]),
     Qs = z_convert:to_binary(z_url:url_encode(Query)),
     Url = <<Endpoint/binary, "?query=", Qs/binary>>,
@@ -42,20 +46,43 @@ query(Endpoint, Query, Headers) ->
             undefined;
         Map ->
             decode(Map)
+    end;
+query(Endpoint, Query, Headers) ->
+    query(Endpoint, sparql_query:query(Query), Headers).
+
+%% @doc Execute query and return result as RDF.
+-spec query_rdf(ginger_uri:uri(), query()) -> [m_rdf:rdf_resource()].
+query_rdf(Endpoint, Query) ->
+    query_rdf(Endpoint, Query, headers()).
+
+%% @doc Execute query and return result as RDF.
+-spec query_rdf(ginger_uri:uri(), query(), map()) -> [m_rdf:rdf_resource()].
+query_rdf(Endpoint, Query, Headers) ->
+    case query(Endpoint, Query, Headers) of
+        undefined ->
+            undefined;
+        #{<<"results">> := #{<<"bindings">> := Bindings}} ->
+            %% Result set can have multiple rows with same ?s (subject URI), so combine those into
+            %% RDF resources.
+            RdfResources = lists:map(
+                fun(#{<<"s">> := #{<<"value">> := Uri}} = Binding) ->
+                    ResolvedBindings = sparql_query:resolve_arguments(Binding, Query),
+                    sparql_result:result_to_rdf(ResolvedBindings, Uri)
+                end,
+                Bindings
+            ),
+            m_rdf:merge(RdfResources)
     end.
 
 %% @doc Get specified properties from a single resource.
 -spec get_resource(url(), url(), [binary()]) -> m_rdf:rdf_resource() | undefined.
 get_resource(Endpoint, Uri, Properties) ->
-    {Query, Arguments} = sparql_query:select_properties(Uri, Properties),
-    case query(Endpoint, Query, #{<<"Accept">> => <<"application/json">>}) of
+    Query = sparql_query:select(Uri, Properties),
+    case query_rdf(Endpoint, Query) of
         undefined ->
-            unfined;
-        %% Take the first bindings row in case multiple rows were returned because of properties
-        %% with multiple values.
-        #{<<"results">> := #{<<"bindings">> := [Bindings | _]}} ->
-            ResolvedBindings = sparql_query:resolve_arguments(Bindings, Arguments),
-            sparql_result:result_to_rdf(ResolvedBindings, Uri)
+            undefined;
+        [Rdf] ->
+            Rdf
     end.
 
 %% @doc Try to decode the response from the SPARQL endpoint.
@@ -68,4 +95,4 @@ decode(Data) ->
     Data.
 
 headers() ->
-    #{<<"Accept">> => <<"application/ld+json">>}.
+    #{<<"Accept">> => <<"application/json">>}.
