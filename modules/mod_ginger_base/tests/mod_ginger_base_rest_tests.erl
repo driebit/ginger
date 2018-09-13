ec8f0e84f015c8555e48e7ea73be2d80f0bf83da
Michel Rijnders <michel@driebit.nl>
2018-09-13 09:29:11 +0200

    Construct wm req instead of using http

--- modules/mod_ginger_base/tests/mod_ginger_base_rest_tests.erl
+++ modules/mod_ginger_base/tests/mod_ginger_base_rest_tests.erl
@@ -6,19 +6,20 @@
 %% -record(state, {mode}).
 
 api_test() ->
+    %% Init
     C = z_context:new(testsandboxdb),
     ok = z_module_manager:activate_await(mod_ginger_base, C),
     Sudo = z_acl:sudo(C),
     %% Create resource
-    {ok, Id1} = m_rsc:insert([{category, text}, {title, <<"Title">>}], Sudo),
-    %% Retrieve and decode resource
-    {ok, Status1, _, _} = hackney:get(rsc_url(Id1), [], <<>>, []),
-    [ ?assertEqual(200, Status1)
+    {ok, Id1} = m_rsc:insert([{category, text}, {title, <<"Title">>}, {published, true}], Sudo),
+    %% Set up request and state
+    Req1 = #wm_reqdata{
+              path_info = dict:from_list([{id, erlang:integer_to_list(Id1)}])
+             },
+    {ok, State1} = controller_rest:init([{mode, document}, {path_info, id}]),
+    {Result1, Req2, State2} = controller_rest:malformed_request(Req1, State1),
+    {Result2, Req3, State3} = controller_rest:resource_exists(Req2, State2),
+    %% Assertions
+    [ ?assertEqual(false, Result1),
+      ?assertEqual(true, Result2)
     ].
-
-rsc_url(Id) ->
-    Id_ = erlang:integer_to_binary(Id),
-    abs_url(<<"/resources/", Id_/binary>>).
-
-abs_url(Path) ->
-    <<"http://localhost:8040/data", Path/binary>>.
