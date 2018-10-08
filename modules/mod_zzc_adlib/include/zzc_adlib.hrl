8ced3c1febd865dae9ea1d252d2283093b52f649
Michel Rijnders <michel@driebit.nl>
2018-10-08 08:37:36 +0200

    Undo previous commit

--- modules/mod_zzc_adlib/include/zzc_adlib.hrl
+++
@@ -1,6 +0,0 @@
-%% @doc Notification that some new/updated data is available in Adlib
--record(adlib_update, {
-    date :: {{pos_integer(), pos_integer(), pos_integer()}, {pos_integer(), pos_integer(), pos_integer()}},
-    record :: map(),
-    database :: binary()
-}).
