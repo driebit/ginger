ef87e6a1d230224ec8eb2b0fa906a7b5fa3af2f2
Michel Rijnders <michel@driebit.nl>
2018-10-08 08:33:08 +0200

    Add "forked" module

---
+++ modules/mod_zzc_adlib/support/zzc_adlib_search.erl
@@ -0,0 +1,27 @@
+-module(zzc_adlib_search).
+
+-export([
+    search/2
+]).
+
+-include("zotonic.hrl").
+
+%% @doc See http://api.adlibsoft.com/site/api/functions/search
+search(#search_query{search = {adlib, Args}, offsetlimit = {From, Size}}, Context) ->
+    Params = [{startfrom, From}, {limit, Size} | Args],
+    case zzc_adlib_client:search(Params, Context) of
+        undefined ->
+            undefined;
+        Response ->
+            search_result(Response)
+    end.
+
+%% @doc Process search result.
+%%      The only way to determine if there are results to be returned, is to
+%%      look for the {"recordList": ...} JSON element.
+-spec search_result(map()) -> #search_result{}.
+search_result(#{<<"adlibJSON">> := #{<<"recordList">> := RecordList, <<"diagnostic">> := #{<<"hits">> := Hits}}}) ->
+    #{<<"record">> := Records} = RecordList,
+    #search_result{result = Records, total = binary_to_integer(Hits)};
+search_result(#{<<"adlibJSON">> := #{<<"diagnostic">> := #{<<"hits">> := Hits}}}) ->
+    #search_result{result = [], total = binary_to_integer(Hits)}.
---
+++ modules/mod_zzc_adlib/templates/admin/adlib_databases.tpl
@@ -0,0 +1,18 @@
+{% if m.config.mod_zzc_adlib.url %}
+    <label class="control-label">{_ Sync databases _}</label>
+    {% with m.config.mod_zzc_adlib.databases.list as current %}
+        {% for database in m.zzc_adlib.listdatabases %}
+            {% with database.database|first as database_name %}
+                {% with "adlib_database_" ++ database.database|first as input_id %}
+                    <div class="checkbox">
+                        <label for="{{ input_id }}">
+                            <input type="checkbox" name="adlib_database" id="{{ input_id }}" value="{{ database_name }}"{% if current|index_of:database_name > 0 %} checked="checked"{% endif %}/>
+                            {% wire id=input_id postback={toggle_database database=database_name} delegate=`m_zzc_adlib` %}
+                            {{ database_name }}
+                        </label>
+                    </div>
+                {% endwith %}
+            {% endwith %}
+        {% endfor %}
+    {% endwith %}
+{% endif %}
