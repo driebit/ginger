c548f79bb89c959bdb0321edf9fd4516247cd92d
rl-king <hello@rubenlieking.com>
2018-10-09 16:27:57 +0200

    Respond with resource and identity on success and 400 on error

--- modules/mod_ginger_base/controllers/controller_auth.erl
+++ modules/mod_ginger_base/controllers/controller_auth.erl
@@ -28,7 +28,7 @@ post_is_create(ReqData, Context) ->
 
 -spec process_post(any(), any()) -> {{atom(), integer()}, any(), z:context()}.
 process_post(ReqData, _) ->
-    {B, _} =  wrq:req_body(ReqData),
+    {B, _} = wrq:req_body(ReqData),
     M = jsx:decode(B, [return_maps, {labels, atom}]),
     U = maps:get(username, M, undefined),
     P = maps:get(password, M, undefined),
@@ -41,33 +41,18 @@ process_post(ReqData, _) ->
                     z_context:set_session(auth_timestamp, calendar:universal_time(), C2),
                     z_context:set_session(auth_user_id, Id, C2),
                     z_context:set_session(user_id, Id, C2),
-                    {{halt, 200}, wrq:set_resp_body(response(true, Id, C2), ReqData), C2};
+                    {{halt, 200}, wrq:set_resp_body(response(Id, C2), ReqData), C2};
                 _ ->
-                    {{halt, 200}, wrq:set_resp_body(response(true, Id, C), ReqData), C}
+                    {{halt, 200}, wrq:set_resp_body(response(Id, C), ReqData), C}
             end;
         {error, _} ->
-            {{halt, 200}, wrq:set_resp_body(response(false, undefined, C), ReqData), C}
+            {{halt, 400}, wrq:set_resp_body(jsx:encode(<<"Error">>), ReqData), C}
     end.
 
--spec response(boolean(), integer(), #context{}) -> binary().
-response(Success, Id, Context) ->
-    {IsVerified, User} =
-        case Success of
-            true ->
-                {m_identity:is_verified(Id, Context), m_ginger_rest:rsc(Id, Context)};
-            false ->
-                {null, null}
-        end,
+-spec response(integer(), #context{}) -> binary().
+response(Id, Context) ->
     Response =
-        #{ <<"loggedin">> => Success,
-           <<"message">> => message(Success),
-           <<"is_verified">> => IsVerified,
-           <<"user">> => User
+        #{<<"identity">> => proplists:delete(propb, m_identity:get(Id, Context)),
+          <<"resource">> => m_ginger_rest:rsc(Id, Context)
          },
     jsx:encode(Response).
-
--spec message(boolean()) -> binary().
-message(false) ->
-    <<"Error">>;
-message(true) ->
-    <<"Successfully logged in">>.
