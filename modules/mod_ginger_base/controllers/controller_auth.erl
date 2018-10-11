17622b368edfec78ff8886d35de1fa2dd66ecf56
rl-king <hello@rubenlieking.com>
2018-10-11 17:07:42 +0200

    Add GET response

--- modules/mod_ginger_base/controllers/controller_auth.erl
+++ modules/mod_ginger_base/controllers/controller_auth.erl
@@ -4,7 +4,9 @@
          init/1,
          allowed_methods/2,
          post_is_create/2,
-         process_post/2
+         process_post/2,
+         content_types_provided/2,
+         to_json/2
         ]).
 
 -include("controller_webmachine_helper.hrl").
@@ -21,6 +23,21 @@ init([]) ->
 -spec allowed_methods(any(), any()) -> {list(), any(), any()}.
 allowed_methods(ReqData, State) ->
     {['POST', 'HEAD', 'GET'], ReqData, State}.
+
+-spec content_types_provided(any(), any()) -> {list(), any(), any()}.
+content_types_provided(Req, State) ->
+    {[{"application/json", to_json}], Req, State}.
+
+-spec to_json(any(), any()) -> {{atom(), integer()}, any(), z:context()}.
+to_json(ReqData, _) ->
+    C = z_context:new(ReqData, ?MODULE),
+    {ok, C2} = z_session_manager:continue_session(C),
+    case z_session:get(auth_user_id, C2) of
+        undefined ->
+            response(400, <<"Error">>, ReqData, C);
+        Id ->
+            response(200, user(Id, C2), C2#context.wm_reqdata, C2)
+    end.
 
 -spec post_is_create(any(), z:context()) -> {boolean(), any(), z:context()}.
 post_is_create(ReqData, Context) ->
@@ -41,18 +58,20 @@ process_post(ReqData, _) ->
                     z_context:set_session(auth_timestamp, calendar:universal_time(), C2),
                     z_context:set_session(auth_user_id, Id, C2),
                     z_context:set_session(user_id, Id, C2),
-                    {{halt, 200}, wrq:set_resp_body(response(Id, C2), ReqData), C2};
+                    response(200, user(Id, C2), C2#context.wm_reqdata, C2);
                 _ ->
-                    {{halt, 200}, wrq:set_resp_body(response(Id, C), ReqData), C}
+                    response(200, user(Id, C), ReqData, C)
             end;
         {error, _} ->
-            {{halt, 400}, wrq:set_resp_body(jsx:encode(<<"Error">>), ReqData), C}
+            response(400, <<"Error">>, ReqData, C)
     end.
 
--spec response(integer(), #context{}) -> binary().
-response(Id, Context) ->
-    Response =
-        #{<<"identity">> => proplists:delete(propb, m_identity:get(Id, Context)),
-          <<"resource">> => m_ginger_rest:rsc(Id, Context)
-         },
-    jsx:encode(Response).
+-spec user(integer(), #context{}) -> map().
+user(Id, Context) ->
+    #{<<"identity">> => proplists:delete(propb, m_identity:get(Id, Context)),
+      <<"resource">> => m_ginger_rest:rsc(Id, Context)
+     }.
+
+-spec response(integer, any(), any(), z:context()) -> {{atom(), integer()}, any(), z:context()}.
+response(Status, Body, ReqData, Context) ->
+    {{halt, Status}, wrq:set_resp_body(jsx:encode(Body), ReqData), Context}.
