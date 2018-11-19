4bf32af36c8539c23ca3ce48a33f99b9e4cfcd70
David de Boer <david@ddeboer.nl>
2018-11-19 18:03:09 +0100

    [base] Fix REST API blocks output (#482)
    * Translated properties were causing jsx:encode to fail.
    * Add test.

---
+++ modules/mod_ginger_base/tests/m_ginger_rest_tests.erl
@@ -0,0 +1,54 @@
+-module(m_ginger_rest_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("zotonic.hrl").
+
+rsc_blocks_test() ->
+    {ok, Id} = m_rsc:insert(
+        [
+            {category, text},
+            {blocks, [
+                [
+                    {type, <<"header">>},
+                    {name, <<"translated_header">>},
+                    {header, {trans, [
+                        {nl, <<"Vertaalde header">>},
+                        {en, <<"Translated header">>}
+                    ]}},
+                    {body, <<"Untranslated body">>}
+                ],
+
+                [
+                    {type, <<"page">>},
+                    {name, <<"page2">>},
+                    {style, <<"quote">>},
+                    {rsc_id, 19013}
+                ]
+            ]}
+        ],
+        context()
+    ),
+    #{<<"blocks">> := [Block1, Block2]} = Rest = m_ginger_rest:rsc(Id, context()),
+    ?assertEqual(
+        [
+            {nl, <<"Vertaalde header">>},
+            {en, <<"Translated header">>}
+        ],
+        maps:get(<<"header">>, Block1)
+    ),
+    ?assertEqual(
+        [
+            {en, <<"Untranslated body">>}
+        ],
+        maps:get(<<"body">>, Block1)
+    ),
+
+    ?assertEqual(<<"page">>, maps:get(<<"type">>, Block2)),
+    ?assertEqual(<<"page2">>, maps:get(<<"name">>, Block2)),
+    ?assertEqual(<<"quote">>, maps:get(<<"style">>, Block2)),
+    ?assertEqual(19013, maps:get(<<"rsc_id">>, Block2)),
+
+    jsx:encode(Rest).
+
+context() ->
+    z_acl:sudo(z_context:new(testsandboxdb)).
