e17bb88dcd7cda4f31fdbf40490dc093561390a1
rl-king <hello@rubenlieking.com>
2018-11-23 17:11:11 +0100

    Init logout
    Add state record with mode and context
    Remove spec

--- modules/mod_ginger_base/controllers/controller_auth.erl
+++ modules/mod_ginger_base/controllers/controller_auth.erl
@@ -1,6 +1,7 @@
 -module(controller_auth).
 
 -export([ init/1
+        , service_available/2
         , allowed_methods/2
         , post_is_create/2
         , process_post/2
@@ -12,29 +13,36 @@
 -include("controller_webmachine_helper.hrl").
 -include("zotonic.hrl").
 
+%% NB: the Webmachine documentation uses "context" where we use "state",
+%% we reserve "context" for the way it's used by Zotonic/Ginger.
+-record(state, { mode = undefined
+               , context = undefined
+               }
+       ).
+
 %%%-----------------------------------------------------------------------------
 %%% Resource functions
 %%%-----------------------------------------------------------------------------
 
--spec init(list()) -> {ok, undefined}.
-init([]) ->
-    {ok, undefined}.
+init([Args]) ->
+    {ok, #state{mode = maps:get(mode, Args)}}.
 
--spec allowed_methods(any(), any()) -> {list(), any(), any()}.
-allowed_methods(ReqData, State) ->
-    {['POST', 'HEAD', 'GET'], ReqData, State}.
+service_available(Req, State) ->
+    Context = z_context:continue_session(z_context:new(Req, ?MODULE)),
+    {true, Req, State#state{context = Context}}.
 
--spec post_is_create(any(), z:context()) -> {boolean(), any(), z:context()}.
-post_is_create(ReqData, Context) ->
-    {false, ReqData, Context}.
+allowed_methods(Req, State) ->
+    {['POST', 'HEAD', 'GET'], Req, State}.
 
--spec process_post(any(), any()) -> {{atom(), integer()}, any(), z:context()}.
-process_post(ReqData, _) ->
-    {B, _} = wrq:req_body(ReqData),
+post_is_create(Req, Context) ->
+    {false, Req, Context}.
+
+process_post(Req, State = #state{mode = login}) ->
+    C = State#state.context,
+    {B, Req1} = wrq:req_body(Req),
     M = jsx:decode(B, [return_maps, {labels, atom}]),
     U = maps:get(username, M, undefined),
     P = maps:get(password, M, undefined),
-    C = z_context:new(ReqData, ?MODULE),
     case m_identity:check_username_pw(U, P, C) of
         {ok, Id} ->
             case z_auth:is_enabled(Id, C) of
@@ -45,37 +53,36 @@ process_post(ReqData, _) ->
                     z_context:set_session(user_id, Id, C2),
                     response(200, user(Id, C2), C2#context.wm_reqdata, C2);
                 _ ->
-                    response(200, user(Id, C), ReqData, C)
+                    response(200, user(Id, C), Req1, C)
             end;
         {error, _} ->
-            response(400, <<"Error">>, ReqData, C)
+            response(400, <<"Error">>, Req1, C)
     end.
 
--spec content_types_provided(any(), any()) -> {list(), any(), any()}.
 content_types_provided(Req, State) ->
     {[{"application/json", to_json}], Req, State}.
 
--spec to_json(any(), any()) -> {{atom(), integer()}, any(), z:context()}.
-to_json(ReqData, _) ->
-    C = z_context:new(ReqData, ?MODULE),
+to_json(Req, State = #state{mode = login}) ->
+    C = State#state.context,
     {ok, C2} = z_session_manager:continue_session(C),
     case z_session:get(auth_user_id, C2) of
         undefined ->
-            response(400, <<"Error">>, ReqData, C);
+            response(400, <<"Error">>, Req, C);
         Id ->
             response(200, user(Id, C2), C2#context.wm_reqdata, C2)
-    end.
+    end;
+to_json(_Req, State = #state{mode = logout}) ->
+    {ok, C1} = z_session_manager:stop_session(State#state.context),
+    response(200, <<>>, C1#context.wm_reqdata, C1).
 
 %%%-----------------------------------------------------------------------------
 %%% Internal functions
 %%%-----------------------------------------------------------------------------
 
--spec user(integer(), #context{}) -> map().
 user(Id, Context) ->
     #{ <<"identity">> => proplists:delete(propb, m_identity:get(Id, Context))
      , <<"resource">> => m_ginger_rest:rsc(Id, Context)
      }.
 
--spec response(integer, any(), any(), z:context()) -> {{atom(), integer()}, any(), z:context()}.
-response(Status, Body, ReqData, Context) ->
-    {{halt, Status}, wrq:set_resp_body(jsx:encode(Body), ReqData), Context}.
+response(Status, Body, Req, Context) ->
+    {{halt, Status}, wrq:set_resp_body(jsx:encode(Body), Req), Context}.
