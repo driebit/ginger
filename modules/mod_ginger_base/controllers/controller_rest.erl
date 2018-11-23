16d634b519cdd661c01e6a3436a32435de60f819
Michel Rijnders <michel@ugly-syntax.org>
2018-11-23 11:45:46 +0100

    [base] Allow creation of edges via the REST API

--- modules/mod_ginger_base/controllers/controller_rest.erl
+++ modules/mod_ginger_base/controllers/controller_rest.erl
@@ -2,11 +2,15 @@
 
 -export([
          init/1,
+         service_available/2,
          malformed_request/2,
+         allowed_methods/2,
          resource_exists/2,
          content_types_provided/2,
-         to_json/2
-        ]).
+         to_json/2,
+         process_post/2
+        ]
+       ).
 
 -include("controller_webmachine_helper.hrl").
 -include("zotonic.hrl").
@@ -14,8 +18,12 @@
 
 %% NB: the Webmachine documenation uses "context" where we use "state",
 %% we reserve "context" for the way it's used by Zotonic/Ginger.
--record(state, {mode, collection, path_info}).
-
+-record(state, { mode = undefined
+               , collection = undefined
+               , path_info = undefined
+               , context = undefined
+               }
+       ).
 
 %%%-----------------------------------------------------------------------------
 %%% Resource functions
@@ -26,6 +34,10 @@ init([Args]) ->
     Collection = maps:get(collection, Args, undefined),
     PathInfo = maps:get(path_info, Args, undefined),
     {ok, #state{mode = Mode, collection = Collection, path_info = PathInfo}}.
+
+service_available(Req, State) ->
+    Context = z_context:continue_session(z_context:new(Req, ?MODULE)),
+    {true, Req, State#state{context = Context}}.
 
 malformed_request(Req, State = #state{mode = collection}) ->
     {false, Req, State};
@@ -41,14 +53,19 @@ malformed_request(Req, State = #state{mode = document, collection = resources, p
             {true, Req, State}
     end.
 
+allowed_methods(Req, State = #state{mode = collection, collection = edges}) ->
+    {['POST', 'HEAD'], Req, State};
+allowed_methods(Req, State) ->
+    {['GET', 'HEAD'], Req, State}.
+
 resource_exists(Req, State = #state{mode = collection}) ->
     {true, Req, State};
 resource_exists(Req, State = #state{mode = document, collection = resources, path_info = id}) ->
-    Context = z_context:new(Req, ?MODULE),
+    Context = State#state.context,
     Id = wrq:path_info(id, Req),
     {m_rsc:exists(Id, Context), Req, State};
 resource_exists(Req, State = #state{mode = document, collection = resources, path_info = path}) ->
-    Context = z_context:new(Req, ?MODULE),
+    Context = State#state.context,
     case path_to_id(wrq:path_info(path, Req), Context) of
         {ok, Id} ->
             {m_rsc:exists(Id, Context), Req, State};
@@ -60,7 +77,7 @@ content_types_provided(Req, State) ->
     {[{"application/json", to_json}], Req, State}.
 
 to_json(Req, State = #state{mode = collection, collection = resources}) ->
-    Context = z_context:new(Req, ?MODULE),
+    Context = State#state.context,
     Args1 = search_query:parse_request_args(
               proplists_filter(
                 fun (Key) -> lists:member(Key, supported_search_args()) end,
@@ -75,7 +92,7 @@ to_json(Req, State = #state{mode = collection, collection = resources}) ->
     Json = jsx:encode([rsc(Id, Context, true) || Id <- Ids]),
     {Json, Req, State};
 to_json(Req, State = #state{mode = document, collection = resources, path_info = PathInfo}) ->
-    Context = z_context:new(Req, ?MODULE),
+    Context = State#state.context,
     Id =
         case PathInfo of
             id ->
@@ -86,6 +103,20 @@ to_json(Req, State = #state{mode = document, collection = resources, path_info =
         end,
     Json = jsx:encode(rsc(Id, Context, true)),
     {Json, Req, State}.
+
+process_post(Req, State = #state{mode = collection, collection = edges}) ->
+    Context = State#state.context,
+    {Body, Req1} = wrq:req_body(Req),
+    Data = jsx:decode(Body, [return_maps, {labels, atom}]),
+    Subject = maps:get(subject, Data),
+    {ok, Predicate} = m_rsc:name_to_id(maps:get(predicate, Data), Context),
+    Object = maps:get(object, Data),
+    case m_edge:insert(Subject, Predicate, Object, Context) of
+        {ok, _EdgeId} ->
+            {true, Req1, State};
+        {error, _Reason} ->
+            {false, Req1, State}
+    end.
 
 %%%-----------------------------------------------------------------------------
 %%% Internal functions
@@ -158,6 +189,28 @@ init_test_() ->
                 collection = State#state.mode,
                 resources = State#state.collection,
                 id = State#state.path_info
+        end
+      ]
+    }.
+
+allowed_methods_test_() ->
+    { setup
+      %% setup
+    , fun () -> ok end
+      %% cleanup
+    , fun (_) -> ok end
+      %% tests
+    , [ fun () ->
+                {Methods1, _, _ } =
+                    allowed_methods(req, #state{mode = collection, collection = edges}),
+                ?assert(lists:member('POST', Methods1)),
+                ?assertNot(lists:member('GET', Methods1)),
+                {Methods2, _, _ } =
+                    allowed_methods(req, #state{mode = collection, collection = resources}),
+                ?assertNot(lists:member('POST', Methods2)),
+                {Methods3, _, _ } =
+                    allowed_methods(req, #state{mode = document}),
+                ?assertNot(lists:member('POST', Methods3))
         end
       ]
     }.
@@ -249,6 +302,46 @@ resource_exists_test_() ->
                 {false, _, _} = resource_exists(req, State),
                 meck:expect(m_rsc, name_to_id, 2, {error, whatever}),
                 {false, _, _} = resource_exists(req, State)
+        end
+      ]
+    }.
+
+process_post_test_() ->
+    { setup
+      %% setup
+    , fun () ->
+              meck:new(wrq),
+              meck:new(m_rsc),
+              meck:new(m_edge),
+              ok
+      end
+      %% cleanup
+    , fun (_) ->
+              meck:unload(m_edge),
+              meck:unload(m_rsc),
+              meck:unload(wrq),
+              ok
+      end
+      %% tests
+    , [ fun () ->
+                State = #state{ mode = collection
+                              , collection = edges
+                              , context = context
+                              },
+                Body = jsx:encode(#{ subject => 1
+                                   , predicate => <<"depiction">>
+                                   , object => 2
+                                   }
+                                 ),
+                meck:expect(wrq, req_body, 1, {Body, req}),
+                PredicateId = 3,
+                meck:expect(m_rsc, name_to_id, 2, {ok, PredicateId}),
+                EdgeId = 4,
+                meck:expect(m_edge, insert, 4, {ok, EdgeId}),
+                {true, _, _} = process_post(req, State),
+                meck:expect(m_edge, insert, 4, {error, {acl, false}}),
+                {false, _, _} = process_post(req, State),
+                ok
         end
       ]
     }.
