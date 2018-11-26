f13089d035b4ef5e168db04ea65ccfe5490e91af
Michel Rijnders <michel@driebit.nl>
2018-11-26 10:43:00 +0100

    Change endpoint for POSTing edges

--- modules/mod_ginger_base/controllers/controller_rest.erl
+++ modules/mod_ginger_base/controllers/controller_rest.erl
@@ -106,10 +106,11 @@ to_json(Req, State = #state{mode = document, collection = resources, path_info =
 
 process_post(Req, State = #state{mode = collection, collection = edges}) ->
     Context = State#state.context,
+    {ok, Subject} = wrq:path_info(id, Req),
+    {ok, Name} = wrq:path_info(predicate, Req),
+    {ok, Predicate} = m_rsc:name_to_id(Name, Context),
     {Body, Req1} = wrq:req_body(Req),
     Data = jsx:decode(Body, [return_maps, {labels, atom}]),
-    Subject = maps:get(subject, Data),
-    {ok, Predicate} = m_rsc:name_to_id(maps:get(predicate, Data), Context),
     Object = maps:get(object, Data),
     case m_edge:insert(Subject, Predicate, Object, Context) of
         {ok, _EdgeId} ->
@@ -328,6 +329,16 @@ process_post_test_() ->
                               , collection = edges
                               , context = context
                               },
+                meck:expect(
+                  wrq,
+                  path_info,
+                  fun
+                      (id, req) ->
+                          {ok, 1};
+                      (predicate, req) ->
+                          {ok, depiction}
+                  end
+                 ),
                 Body = jsx:encode(#{ subject => 1
                                    , predicate => <<"depiction">>
                                    , object => 2
