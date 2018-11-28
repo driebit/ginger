8c8c49131d3193731c41eb29f7c3e77c0776443f
Michel Rijnders <michel@driebit.nl>
2018-11-28 11:44:33 +0100

    Run `controller_rest` tests on CI too

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-MODULES="$(ls ebin/*ginger*tests.beam|sed -e 's|ebin/||'|sed -e 's|.beam||')"
+MODULES="controller_rest $(ls ebin/*ginger*tests.beam|sed -e 's|ebin/||'|sed -e 's|.beam||')"
 bin/zotonic runtests $MODULES
