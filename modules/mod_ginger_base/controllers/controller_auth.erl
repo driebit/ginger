340ae5020f2515b8fffeb1f4dbed1aac18f57502
Michel Rijnders <michel@ugly-syntax.org>
2018-12-12 10:58:37 +0100

    Clean up auth controller (#504)

--- modules/mod_ginger_base/controllers/controller_auth.erl
+++ modules/mod_ginger_base/controllers/controller_auth.erl
@@ -30,20 +30,21 @@ post_is_create(Req, Context) ->
     {false, Req, Context}.
 
 process_post(Req, State = #state{mode = login}) ->
-    C = z_context:new(Req, ?MODULE),
-    {B, Req1} = wrq:req_body(Req),
-    M = jsx:decode(B, [return_maps, {labels, atom}]),
-    U = maps:get(username, M, undefined),
-    P = maps:get(password, M, undefined),
-    case m_identity:check_username_pw(U, P, C) of
+    Context = z_context:new(Req, ?MODULE),
+    {Body, Req1} = wrq:req_body(Req),
+    Data = jsx:decode(Body, [return_maps, {labels, atom}]),
+    Username = maps:get(username, Data, undefined),
+    Password = maps:get(password, Data, undefined),
+    case m_identity:check_username_pw(Username, Password, Context) of
         {ok, Id} ->
-            case z_auth:is_enabled(Id, C) of
+            case z_auth:is_enabled(Id, Context) of
                 true ->
-                    {ok, C2} = z_session_manager:start_session(ensure, C#context.session_id, C),
-                    z_context:set_session(auth_timestamp, calendar:universal_time(), C2),
-                    z_context:set_session(auth_user_id, Id, C2),
-                    z_context:set_session(user_id, Id, C2),
-                    Req2 = wrq:set_resp_body(jsx:encode(user(Id, C2)), C2#context.wm_reqdata),
+                    {ok, Context2} =
+                        z_session_manager:start_session(ensure, Context#context.session_id, Context),
+                    z_context:set_session(auth_timestamp, calendar:universal_time(), Context2),
+                    z_context:set_session(auth_user_id, Id, Context2),
+                    z_context:set_session(user_id, Id, Context2),
+                    Req2 = wrq:set_resp_body(jsx:encode(user(Id, Context2)), Context2#context.wm_reqdata),
                     {true, Req2, State};
                 _ ->
                     {{halt, 400}, Req1, State}
@@ -52,22 +53,22 @@ process_post(Req, State = #state{mode = login}) ->
             {{halt, 400}, Req1, State}
     end;
 process_post(Req, State = #state{mode = logout}) ->
-    C = z_context:new(Req, ?MODULE),
-    {ok, C2} = z_session_manager:continue_session(C),
-    {ok, C3} = z_session_manager:stop_session(C2),
-    {{halt, 204}, C3#context.wm_reqdata, State}.
+    Context = z_context:new(Req, ?MODULE),
+    {ok, Context2} = z_session_manager:continue_session(Context),
+    {ok, Context3} = z_session_manager:stop_session(Context2),
+    {{halt, 204}, Context3#context.wm_reqdata, State}.
 
 content_types_provided(Req, State) ->
     {[{"application/json", to_json}], Req, State}.
 
 to_json(Req, State = #state{mode = status}) ->
-    {ok, C} = z_session_manager:continue_session(z_context:new(Req, ?MODULE)),
-    case z_session:get(user_id, C) of
+    {ok, Context} = z_session_manager:continue_session(z_context:new(Req, ?MODULE)),
+    case z_session:get(user_id, Context) of
         none ->
-            {{halt, 400}, C#context.wm_reqdata, State};
+            {{halt, 400}, Context#context.wm_reqdata, State};
         Id ->
-            Body = jsx:encode(user(Id, C)),
-            {Body, C#context.wm_reqdata, State}
+            Body = jsx:encode(user(Id, Context)),
+            {Body, Context#context.wm_reqdata, State}
     end.
 
 %%%-----------------------------------------------------------------------------
