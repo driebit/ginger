1e98c677cb5bfc81a60abe60ea9ffe569a6ae251
rl-king <hello@rubenlieking.com>
2019-03-01 10:30:42 +0100

    Handle missing category field as it's required
    Without this check the response code will be a 500 with no information about the
    actual problem whatsoever.
    
    We now return a 400 and log an error with the problem.

--- modules/mod_ginger_base/controllers/controller_rest_resources.erl
+++ modules/mod_ginger_base/controllers/controller_rest_resources.erl
@@ -99,22 +99,28 @@ process_post(Req, State = #state{mode = collection}) ->
     %% Create resource
     {Body, Req1} = wrq:req_body(Req),
     Data = jsx:decode(Body, [return_maps, {labels, atom}]),
-    Props = lists:foldl(fun post_props/2, [], maps:to_list(Data)),
-    {ok, Id} = m_rsc:insert(Props, Context),
-    %% Create edges
-    lists:foreach(
-      fun (Edge) ->
-              {ok, PredicateId} = m_rsc:name_to_id(maps:get(predicate, Edge), Context),
-              {ok, _EdgeId} = m_edge:insert(Id, PredicateId, maps:get(object, Edge), Context)
+    case maps:get(category, Data, undefined) of
+        undefined ->
+            lager:error("Category is missing from the request body", []),
+            {{halt, 400}, Req1, State};
+        _Cat ->
+            Props = lists:foldl(fun post_props/2, [], maps:to_list(Data)),
+            {ok, Id} = m_rsc:insert(Props, Context),
+            %% Create edges
+            lists:foreach(
+              fun (Edge) ->
+                      {ok, PredicateId} = m_rsc:name_to_id(maps:get(predicate, Edge), Context),
+                      {ok, _EdgeId} = m_edge:insert(Id, PredicateId, maps:get(object, Edge), Context)
 
-      end,
-      maps:get(edges, Data, [])
-     ),
-    %% Set "Location" header
-    Location = "/data/resources/" ++ erlang:integer_to_list(Id),
-    Req2 = wrq:set_resp_headers([{"Location", Location}], Req1),
-    %% Done
-    {{halt, 201}, Req2, State}.
+              end,
+              maps:get(edges, Data, [])
+             ),
+            %% Set "Location" header
+            Location = "/data/resources/" ++ erlang:integer_to_list(Id),
+            Req2 = wrq:set_resp_headers([{"Location", Location}], Req1),
+            %% Done
+            {{halt, 201}, Req2, State}
+    end.
 
 %%%-----------------------------------------------------------------------------
 %%% Internal functions
