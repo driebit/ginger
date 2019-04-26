af853f8dcb55fbbc24808bab9abddfab8c9a277a
rl-king <hello@rubenlieking.com>
2019-04-26 16:34:52 +0200

    Init g_string

---
+++ modules/mod_ginger_base/support/g_string.erl
@@ -0,0 +1,32 @@
+%%%-------------------------------------------------------------------
+%%% @author driebit <tech@driebit.nl>
+%%% @copyright (C) 2019, driebit
+%%% @doc
+%%% Utility functions for operations on strings
+%%% @end
+%%%-------------------------------------------------------------------
+-module(g_string).
+
+%% API
+-export([contains/2]).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%% @doc Check if a string contains another string
+-spec contains(list(), list()) -> boolean().
+contains(_, []) ->
+    false;
+contains(Pred, Str) ->
+    case lists:prefix(Pred, Str) of
+        true->
+            true;
+        false ->
+            contains(Pred, lists:nthtail(1, Str))
+    end.
+
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
