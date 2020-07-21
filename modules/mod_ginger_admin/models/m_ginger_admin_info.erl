8e33bb8d0c5661949870b53045713d9bc8912e3c
X.R. van der Goot <xander@driebit.nl>
2020-07-21 12:54:15 +0200

    WIP: admin info panel

---
+++ modules/mod_ginger_admin/models/m_ginger_admin_info.erl
@@ -0,0 +1,51 @@
+-module(m_ginger_admin_info).
+-behaviour(gen_model).
+
+-export([m_find_value/3,
+         m_to_list/2, m_value/2,
+         git_ginger_info/0, erlang_info/0, git_zotonic_info/0]).
+
+-include_lib("zotonic.hrl").
+
+
+m_value(T, _Context) ->
+    T.
+
+m_find_value(git_zotonic_info, #m{value=undefined}, _) ->
+    git_zotonic_info();
+m_find_value(git_ginger_info, #m{value=undefined}, _) ->
+    git_ginger_info();
+m_find_value(erlang_info, #m{value=undefined}, _) ->
+    erlang_info();
+m_find_value(directory_info, #m{value=undefined}, _) ->
+    directory_info();
+m_find_value(env_info, #m{value=undefined}, _) ->
+    env_info().
+
+
+
+m_to_list(_,_) ->
+    [].
+
+directory_info() ->
+    {ok, Path} = file:get_cwd(),
+    Path.
+
+env_info() ->
+   os:getenv().
+
+erlang_info() ->
+    erlang:system_info(system_version).
+
+git_zotonic_info() ->
+    case os:find_executable("git") of
+       false -> "Could not find Git";
+       _Path -> os:cmd("(cd ../zotonic; git describe --tags)")
+    end.
+
+git_ginger_info() ->
+    case os:find_executable("git") of
+       false -> "Could not find Git";
+       _Path -> os:cmd("(cd ../ginger; git describe --tags)")
+    end.
+
