60d537d9467f7f6f50fe3dbdd1ab182d7722145e
Colin de Roos <8250462+cdfa@users.noreply.github.com>
2022-09-09 15:23:50 +0200

    Add ginger_seach_hash scomp (#713)
    Co-authored-by: Colin de Roos <colin@driebit.nl>

---
+++ modules/mod_ginger_base/scomps/scomp_ginger_base_ginger_search_hash.erl
@@ -0,0 +1,13 @@
+-module(scomp_ginger_base_ginger_search_hash).
+-behaviour(gen_scomp).
+-export([vary/2, render/3]).
+-include_lib("zotonic.hrl").
+
+vary(_Params, _Context) -> nocache.
+
+% Scomp for linking to search pages with some filters enabled.
+% Example: `<a href="{{ m.rsc.knowledgebase_query.page_url }}#{% ginger_search_hash hassubjects=[blk.rsc_id] type="list"|to_binary %}">`
+% Note that adding a `type` is required
+render(Params, _Vars, Context) ->
+    Hash = base64:encode(jsx:encode(maps:from_list(Params))),
+    {ok, z_render:render(Hash, Context)}.
