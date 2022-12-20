a880749a25054f2bfdc19523892b6219d4ccddd5
Marc Worrell <marc@worrell.nl>
2022-12-20 13:06:42 +0100

    mod_ginger_adlib_elasticearch2: add adlib delete index button. (#725)
    * mod_ginger_adlib_elasticearch2: add adlib delete index button.
    
    Handle new return values from elasticsearch2:get_doc
    
    * Update _admin_status.tpl
    
    typo
    
    Co-authored-by: loetie <loetie@driebit.nl>

--- modules/mod_ginger_adlib_elasticsearch2/mod_ginger_adlib_elasticsearch2.erl
+++ modules/mod_ginger_adlib_elasticsearch2/mod_ginger_adlib_elasticsearch2.erl
@@ -9,15 +9,30 @@
 -mod_schema(1).
 
 -export([
+    event/2,
+
     init/1,
     index/1,
     types/1,
     manage_schema/2,
-    observe_adlib_update/2
+    observe_adlib_update/2,
+
+    prepare_elasticsearch_index/1,
+    delete_recreate_elasticsearch_index/1
 ]).
 
 -include_lib("mod_ginger_adlib/include/ginger_adlib.hrl").
 -include_lib("zotonic.hrl").
+
+
+event(#postback{ message={delete_index, _Args}}, Context) ->
+    case z_acl:is_admin(Context) of
+        true ->
+            delete_recreate_elasticsearch_index(Context),
+            z_render:growl(?__("Index has been recreated. Reimport Adlib documents to fill it.", Context), Context);
+        false ->
+            z_render:growl_error(?__("You need to be an admin to delete the Adlib Elastic Search index.", Context), Context)
+    end.
 
 init(Context) ->
     default_config(index, index(Context), Context),
@@ -32,6 +47,14 @@ prepare_elasticsearch_index(Context) ->
     {Version, Mapping} = ginger_adlib_elasticsearch2_index_mapping:default_mapping(Context),
     {ok, _} = elasticsearch2_index:upgrade(index(Context), Mapping, Version, Context),
     ok.
+
+%% @doc Delete and recreate index and mappings for each type in the index.
+-spec delete_recreate_elasticsearch_index(z:context()) -> ok.
+delete_recreate_elasticsearch_index(Context) ->
+    {Version, Mapping} = ginger_adlib_elasticsearch2_index_mapping:default_mapping(Context),
+    {ok, _} = elasticsearch2_index:delete_recreate(index(Context), Mapping, Version, Context),
+    ok.
+
 
 observe_adlib_update(#adlib_update{date = _Date, database = Database, record = #{<<"@attributes">> := #{<<"priref">> := Priref}} = Record}, Context) ->
     lager:debug("Indexing Adlib record ~s from database ~s", [Priref, Database]),
